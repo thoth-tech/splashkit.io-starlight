8a01de8ae08bdbebfb8ce5718b19cf342ee39557
kay-kaushik <kartikkaushik116@gmail.com>
2024-05-17 23:19:41 +1000

    kartik kaushik usage examples

---
+++ src/assets/usage-examples-code/sprites/draw_sprite/draw_sprite-1-drawing-sprite.cpp
@@ -0,0 +1,33 @@
+/**
+ * Usage Example: drawing a sprite
+ **/
+
+#include "splashkit.h"
+
+int main()
+{
+    // window to draw the sprite on
+    window start = open_window("create_sprite", 600, 600);
+
+    // bitmap for creating a sprite
+    bitmap player = load_bitmap("playerBmp", "protSpriteSheet.png");
+    bitmap_set_cell_details(player, 31, 32, 4, 3, 12);
+
+    // creating the player sprite
+    sprite player_sprite = create_sprite(player);
+
+    // setting the co-ordinates in refrence to the window
+    sprite_set_x(player_sprite, 300);
+    sprite_set_y(player_sprite, 300);
+    
+    // clearing the screen
+    clear_screen(COLOR_BLACK);
+
+    //drawing the sprite
+    draw_sprite(player_sprite);
+
+    refresh_screen();
+    delay(10000);
+
+    return 0;
+}
