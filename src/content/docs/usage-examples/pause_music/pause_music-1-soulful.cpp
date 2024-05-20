75ac24c7ee6f8ec10144f630b2260951d40336f7
DerDieDasDeakin <fjv2000@live.com.au>
2024-05-20 21:46:48 +0930

    Pause_Music_In C++ Usage Example
    This PR offers a usage example in C++ for pause_music.

---
+++ src/content/docs/usage-examples/pause_music/pause_music-1-soulful.cpp
@@ -0,0 +1,19 @@
+#include "splashkit.h"
+
+
+int main()
+{
+    //Loads the music, we give it a name we can use to access it easily and then we add the file itself.
+    load_music("music", "soulful_streets.mp3");
+    //We then call which music file we want and how many milliseconds it should take to fade in to full volume. 
+    fade_music_in("music", 4000);
+    //Delay to hear the music
+    delay(2000);
+    //Pauses the music
+    pause_music();
+    delay(2000);
+   
+    return 0;
+
+
+}
