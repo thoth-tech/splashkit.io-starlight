fcfede3b278a81955891bb455da19df2c3141bd8
edwinshibu <44899696+edwinshibu@users.noreply.github.com>
2024-05-22 22:52:21 +1000

    Update to_lowercase-1-basic-usage.cpp

--- src/assets/usage-examples-code/utilities/to_lowercase/to_lowercase-1-basic-usage.cpp
+++ src/assets/usage-examples-code/utilities/to_lowercase/to_lowercase-1-basic-usage.cpp
@@ -5,7 +5,7 @@ int main()
 {
     std::string inputText;
     std::cout << "Enter a text: ";
-    std::getline(std::cin, inputText); // Read user input
+    std::getline(std::cin, inputText); 
     std::string lowerCaseText = to_lowercase(inputText);
     std::cout << "Original text: " << inputText << std::endl;
     std::cout << "Lowercase text: " << lowerCaseText << std::endl;
