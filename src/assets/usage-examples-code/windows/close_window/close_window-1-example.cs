29808c65f34658be6ccd85ef64de5436d7338338
Avenue <ajoju@deakin.edu.au>
2024-05-25 15:40:18 +1000

    Usage example for close_window

---
+++ src/assets/usage-examples-code/windows/close_window/close_window-1-example.cs
@@ -0,0 +1,12 @@
+﻿using static SplashKitSDK.SplashKit;  
+using SplashKitSDK; 
+
+// Open a window
+Window myWindow = OpenWindow("Close Window Example", 800, 600);
+
+// Display a message and wait for a few seconds
+WriteLine("The window will close in 5 seconds...");
+Delay(5000);
+
+// Close the window
+CloseWindow(myWindow);
