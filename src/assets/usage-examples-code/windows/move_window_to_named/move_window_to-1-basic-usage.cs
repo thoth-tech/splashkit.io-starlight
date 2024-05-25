739d61af9bc28ef019693c48bc10892373b3ae92
benmathewthomas <88536879+benmathewthomas@users.noreply.github.com>
2024-05-26 00:49:53 +1000

    Usage example for move_window_to_named function

---
+++ src/assets/usage-examples-code/windows/move_window_to_named/move_window_to-1-basic-usage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using SplashKitSDK;
+
+string windowName = "Move Named Window Example";
+Window wind = SplashKit.OpenWindow(windowName, 800, 600);
+
+Console.WriteLine("Moving the window '" + windowName + "' to (100, 100)...");
+
+SplashKit.MoveWindowTo(windowName, 100, 100);
+
+Console.WriteLine("Window '" + windowName + "' moved to (100, 100).");
