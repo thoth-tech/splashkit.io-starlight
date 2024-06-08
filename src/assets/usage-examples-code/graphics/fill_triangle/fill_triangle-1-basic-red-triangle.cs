bbf7728ccc72a65fb3298ff88b29b53a395c19de
Olivia McKeon <o.mckeon@deakin.edu.au>
2024-06-08 13:37:30 +1000

    Update and rename fill_triangle-2-basic-red-triangle.cs to fill_triangle-1-basic-red-triangle.cs

--- src/assets/usage-examples-code/graphics/fill_triangle/fill_triangle-1-basic-red-triangle.cs
+++ src/assets/usage-examples-code/graphics/fill_triangle/fill_triangle-1-basic-red-triangle.cs
@@ -7,7 +7,7 @@ OpenWindow("Fill Triangle Example", 800, 600);
 ClearScreen(ColorWhite());
 
 // Fill a triangle with vertices (100, 100), (200, 200), and (300, 100) with red color
-FillTriangle(Color.Red, 100, 100, 200, 200, 300, 100);
+FillTriangle(ColorRed(), 100, 100, 200, 200, 300, 100);
 
 // Refresh the screen to display the filled triangle
 RefreshScreen();
@@ -17,4 +17,3 @@ Delay(5000);
 
 // Close all windows
 CloseAllWindows();
-
