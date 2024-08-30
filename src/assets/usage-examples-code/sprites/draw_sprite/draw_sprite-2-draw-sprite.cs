17f0594cde577c63b5301f9bf9e6464d625555c5
kay-kaushik <kartikkaushik116@gmail.com>
2024-08-30 12:24:48 +1000

    fixed spelling errors and code, added sprite png

--- src/assets/usage-examples-code/sprites/draw_sprite/draw_sprite-2-draw-sprite.cs
+++ src/assets/usage-examples-code/sprites/draw_sprite/draw_sprite-2-draw-sprite.cs
@@ -5,33 +5,27 @@
 using System;
 using SplashKitSDK;
 
-public class Program
-{
-    public static void Main()
-    {
-        // window to draw the sprite on
-        Window start = SplashKit.OpenWindow("create_sprite", 600, 600);
+// window to draw the sprite on
+Window start = SplashKit.OpenWindow("create_sprite", 600, 600);
 
-        // bitmap for creating a sprite
-        Bitmap player = SplashKit.LoadBitmap("playerBmp", "protSpriteSheet.png");
-        player.SetCellDetails(31, 32, 4, 3, 12);
+// bitmap for creating a sprite
+Bitmap player = SplashKit.LoadBitmap("playerBmp", "protSpriteSheet.png");
+player.SetCellDetails(31, 32, 4, 3, 12);
 
-        // creating the player sprite
-        Sprite playerSprite = SplashKit.CreateSprite(player);
+// creating the player sprite
+Sprite playerSprite = SplashKit.CreateSprite(player);
 
-        // setting the coordinates in reference to the window
-        playerSprite.X = 300;
-        playerSprite.Y = 300;
+// setting the coordinates in reference to the window
+playerSprite.X = 300;
+playerSprite.Y = 300;
 
-        // clearing the screen
-        SplashKit.ClearScreen(Color.Black);
+// clearing the screen
+SplashKit.ClearScreen(Color.Black);
 
-        // drawing the sprite
-        SplashKit.DrawSprite(playerSprite);
+// drawing the sprite
+SplashKit.DrawSprite(playerSprite);
 
-        SplashKit.RefreshScreen();
-        SplashKit.Delay(10000);
+SplashKit.RefreshScreen();
+SplashKit.Delay(10000);
 
-        SplashKit.CloseWindow(start);
-    }
-}
+SplashKit.CloseWindow(start);
