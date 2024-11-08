38d6fc7b0c7968cff0f59a1ce8deddf121e5341d
SimonRhook <rhooki13@gmail.com>
2024-11-08 00:14:44 +0000

    Added C# OOP and python updated C# top level file name for write_line_int

---
+++ public/usage-examples/terminal/write_line_int/write_line_int-1-simple.py
@@ -0,0 +1,22 @@
+from splashkit import *
+
+# Example 1: Print single integer
+write_line(1)
+write_line(2)
+write_line(3)
+write_line(-1)
+write_line(-2)
+write_line(-3)
+
+# Example 2: Print multi-digit integer
+write_line(12345)
+write_line(953221311)
+write_line(-165746)
+
+# Example 3: Print integer after calculation
+a = 222-111
+b = 10 * 12
+c = 100 / 5
+write_line(a - b)
+write_line(b)
+write_line(c)
