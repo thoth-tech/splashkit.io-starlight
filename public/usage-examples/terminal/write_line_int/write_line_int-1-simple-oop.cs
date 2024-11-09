52f7402b3d1c7dd81ed4e66ef5e0985ed9cf935e
SimonRhook <127938862+SimonRhook@users.noreply.github.com>
2024-11-09 13:50:40 +1100

    Update write_line_int-1-simple-oop.cs
    Updated OOP namespace issue as suggested

--- public/usage-examples/terminal/write_line_int/write_line_int-1-simple-oop.cs
+++ public/usage-examples/terminal/write_line_int/write_line_int-1-simple-oop.cs
@@ -7,26 +7,26 @@ namespace WriteLineInt
         public static void Main()
         {
             // Example 1: Print single integer
-            WriteLine(1);
-            WriteLine(2);
-            WriteLine(3);
-            WriteLine(-1);
-            WriteLine(-2);
-            WriteLine(-3);
+            SplashKit.WriteLine(1);
+            SplashKit.WriteLine(2);
+            SplashKit.WriteLine(3);
+            SplashKit.WriteLine(-1);
+            SplashKit.WriteLine(-2);
+            SplashKit.WriteLine(-3);
 
             // Example 2: Print multi-digit integer
-            WriteLine(12345);
-            WriteLine(953221311);
-            WriteLine(-165746);
+            SplashKit.WriteLine(12345);
+            SplashKit.WriteLine(953221311);
+            SplashKit.WriteLine(-165746);
 
             // Example 3: Print integer after calculation
             int a = 222-111;
             int b = 10 * 12;
             int c = 100 / 5;
 
-            WriteLine(a - b);
-            WriteLine(b);
-            WriteLine(c);
+            SplashKit.WriteLine(a - b);
+            SplashKit.WriteLine(b);
+            SplashKit.WriteLine(c);
         }
     }
 }
