4b1d3eb174df7e4dfcafb0b876d599f38cb1cb8c
SimonRhook <rhooki13@gmail.com>
2024-11-13 04:36:56 +0000

    Simplified implementation

--- public/usage-examples/graphics/draw_quad/draw_quad-1-simple-top-level.cs
+++ public/usage-examples/graphics/draw_quad/draw_quad-1-simple-top-level.cs
@@ -2,38 +2,20 @@ using SplashKitSDK;
 using static SplashKitSDK.SplashKit;
 
 
-//Initialise quad coords
-Quad q1 = new Quad();
-Quad q2 = new Quad();
-Quad q3 = new Quad();
-Quad q4 = new Quad();
-q1.Points = new Point2D[4];
-q2.Points = new Point2D[4];
-q3.Points = new Point2D[4];
-q4.Points = new Point2D[4];
+//Initialise quads with x1,y1,...,x4,y4
+Quad q1 = QuadFrom(400,200,300,300,300,0,200,200);   
+Quad q2 = QuadFrom(400,210,310, 300,600,300,400,390);
+Quad q3 = QuadFrom(200,400,300, 300,300,600,400,400);
+Quad q4 = QuadFrom(200,390,290, 300,0,300,200,210);   
 
-double[,] q1_corners = new double[,]{{400,200},{300, 300},{300,0},{200,200}}; 
-double[,] q2_corners = new double[,]{{400,210},{310, 300},{600,300},{400,390}}; 
-double[,] q3_corners = new double[,]{{200,400},{300, 300},{300,600},{400,400}}; 
-double[,] q4_corners = new double[,]{{200,390},{290, 300},{0,300},{200,210}}; 
-
-
-
-// set coords for quad
-for (int i = 0; i < 4; i++) {
-    q1.Points[i] = PointAt(q1_corners[i,0],q1_corners[i,1]);
-    q2.Points[i] = PointAt(q2_corners[i,0],q2_corners[i,1]);
-    q3.Points[i] = PointAt(q3_corners[i,0],q3_corners[i,1]);
-    q4.Points[i] = PointAt(q4_corners[i,0],q4_corners[i,1]);
-}
 
 // Create Window
 
 OpenWindow("Draw Quad", 600, 600);
-ClearScreen(ColorBlack());
+ClearScreen(ColorWhite());
 
 
-DrawQuad(ColorWhite(), q1);
+DrawQuad(ColorBlack(), q1);
 DrawQuad(ColorGreen(), q2);
 DrawQuad(ColorRed(), q3);
 DrawQuad(ColorBlue(), q4);
