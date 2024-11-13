67f918164cd0178b58237034bdc78423fa3e7860
SimonRhook <rhooki13@gmail.com>
2024-11-13 04:34:54 +0000

    Simplified implementation

--- public/usage-examples/graphics/draw_quad_on_window/draw_quad_on_window-1-simple.cpp
+++ public/usage-examples/graphics/draw_quad_on_window/draw_quad_on_window-1-simple.cpp
@@ -2,24 +2,12 @@
 
 int main()
 {
-    // Initialise quad coords
-    double q1_corners[4][2] = {{400,200},{300, 300},{300,0},{200,200}};
-    double q2_corners[4][2] = {{400,210},{310, 300},{600,300},{400,390}};
-    double q3_corners[4][2] = {{200,400},{300, 300},{300,600},{400,400}};
-    double q4_corners[4][2] = {{200,390},{290, 300},{0,300},{200,210}};
-    quad q1;
-    quad q2;
-    quad q3;
-    quad q4;
-
-    // set coords
-    for (int i = 0; i < 4; i++) {
-        q1.points[i] = point_at(q1_corners[i][0],q1_corners[i][1]);
-        q2.points[i] = point_at(q2_corners[i][0],q2_corners[i][1]);
-        q3.points[i] = point_at(q3_corners[i][0],q3_corners[i][1]);
-        q4.points[i] = point_at(q4_corners[i][0],q4_corners[i][1]);
-    }
-
+    // Initialise quads with x1,y1,...,x4,y4
+    quad q1 = quad_from(400,200,300,300,300,0,200,200);
+    quad q2 = quad_from(400,210,310, 300,600,300,400,390);
+    quad q3 = quad_from(200,400,300, 300,300,600,400,400);
+    quad q4 = quad_from(200,390,290, 300,0,300,200,210);
+    
     // Create Window
     window window1 = open_window("Draw Quad On Window 1", 600, 600);
     window window2 = open_window("Draw Quad On Window 2", 600, 600);
--- public/usage-examples/graphics/draw_quad_on_window/draw_quad_on_window-1-simple.py
+++ public/usage-examples/graphics/draw_quad_on_window/draw_quad_on_window-1-simple.py
@@ -1,28 +1,16 @@
 from splashkit import *
 
-# Initialise quad coords
-q1_corners = ([400,200],[300, 300],[300,0],[200,200])
-q2_corners = ([400,210],[310, 300],[600,300],[400,390])
-q3_corners = ([200,400],[300, 300],[300,600],[400,400])
-q4_corners = ([200,390],[290, 300],[0,300],[200,210])
+# Initialise quads with x1,y1,...,x4,y4
+q1 = quad_from(400,200,300,300,300,0,200,200)
+q2 = quad_from(400,210,310, 300,600,300,400,390)
+q3 = quad_from(200,400,300, 300,300,600,400,400)
+q4 = quad_from(200,390,290, 300,0,300,200,210)
 
-q1 = Quad()
-q2 = Quad()
-q3 = Quad()
-q4 = Quad()
-
-# set coords
-for i in range(4):
-    q1._points[i] = point_at(q1_corners[i][0],q1_corners[i][1])
-    q2._points[i] = point_at(q2_corners[i][0],q2_corners[i][1])
-    q3._points[i] = point_at(q3_corners[i][0],q3_corners[i][1])
-    q4._points[i] = point_at(q4_corners[i][0],q4_corners[i][1])
-    
 
 # Create Window
 
-window1 = open_window("Draw Quad", 600, 600)
-window2 = open_window("Draw Quad", 600, 600)
+window1 = open_window("Draw Quad On Window 1", 600, 600)
+window2 = open_window("Draw Quad On Window 2", 600, 600)
 clear_screen(color_white())
 
 
