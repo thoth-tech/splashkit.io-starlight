de3f7966ff2bfbc17493a546e413e886babaf350
EthanRin <sovanpanhaarinn@gmail.com>
2024-11-14 23:43:32 +1100

    fill triangle on bitmap example updates

---
+++ public/usage-examples/graphics/fill_triangle_on_bitmap/fill_triangle_on_bitmap-1-simple-oop.cs
@@ -0,0 +1,47 @@
+using SplashKitSDK;
+
+namespace Program
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            //Open a Window
+            SplashKit.OpenWindow("Happy Hat", 618, 618);
+
+            //Load the bitmaps for sad and smiling emojis
+            Bitmap sadEmoji = SplashKit.LoadBitmap("sad_emoji", "sad_emoji.png");
+            Bitmap smilingEmoji = SplashKit.LoadBitmap("smiling_emoji", "smiling_emoji.png");
+
+            // Draw the sad emoji and add a hat
+            SplashKit.ClearScreen(SplashKit.ColorBlack());
+            SplashKit.DrawBitmap(sadEmoji, 0, 0);
+            SplashKit.RefreshScreen();
+            SplashKit.Delay(1000);
+
+            // Draw a triangle hat on the smiling emoji
+            SplashKit.FillTriangleOnBitmap(smilingEmoji, SplashKit.ColorRed(), 100, 200, 309, 20, 520, 200);
+            
+            // Clear Screen and switch to smiling emoji
+            SplashKit.ClearScreen(SplashKit.ColorBlack());
+            SplashKit.DrawBitmap(smilingEmoji, 0, 0);
+            SplashKit.RefreshScreen();
+            SplashKit.Delay(1000);
+
+            // Spin the smiling emoji with the hat
+            for (int i = 0; i < 360; i++)
+            {
+                SplashKit.ClearScreen(SplashKit.ColorBlack());
+                SplashKit.DrawBitmap(smilingEmoji, 0, 0, SplashKit.OptionRotateBmp(i));
+                SplashKit.RefreshScreen();
+                SplashKit.Delay(10);
+            }
+
+            // Free the bitmap resource
+            SplashKit.FreeAllBitmaps();
+            // CLose all windows
+            SplashKit.CloseAllWindows();
+
+        }
+    }
+}
