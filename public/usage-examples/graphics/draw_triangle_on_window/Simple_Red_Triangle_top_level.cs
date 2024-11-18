73336cb009cd1d959583d884f1daa023e3210295
huyhuynh2319 <huyhuynh2319@gmail.com>
2024-11-18 15:40:06 +1100

    add

---
+++ public/usage-examples/graphics/draw_triangle_on_window/Simple_Red_Triangle_top_level.cs
@@ -0,0 +1,9 @@
+﻿using static SplashKitSDK.SplashKit;
+
+OpenWindow("Simple Red Triangle", 800, 600);
+{
+    ProcessEvents();
+    ClearScreen(Color.White);
+    FillTriangle(Color.Red, 150, 50, 250, 250, 50, 250);
+
+}
