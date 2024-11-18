2861889524719711a4cd4276a23ab56fa4f2b507
EthanRin <sovanpanhaarinn@gmail.com>
2024-11-18 12:18:36 +1100

    api updates to link usage example

--- scripts/components.cjs
+++ scripts/components.cjs
@@ -44,6 +44,62 @@ var name = "";
 
 const sk_colors = ["alice_blue", "antique_white", "aqua", "aquamarine", "azure", "beige", "bisque", "black", "blanched_almond", "blue", "blue_violet", "bright_green", "brown", "burly_wood", "cadet_blue", "chartreuse", "chocolate", "coral", "cornflower_blue", "cornsilk", "crimson", "cyan", "dark_blue", "dark_cyan", "dark_goldenrod", "dark_gray", "dark_green", "dark_khaki", "dark_magenta", "dark_olive_green", "dark_orange", "dark_orchid", "dark_red", "dark_salmon", "dark_sea_green", "dark_slate_blue", "dark_slate_gray", "dark_turquoise", "dark_violet", "deep_pink", "deep_sky_blue", "dim_gray", "dodger_blue", "firebrick", "floral_white", "forest_green", "fuchsia", "gainsboro", "ghost_white", "gold", "goldenrod", "gray", "green", "green_yellow", "honeydew", "hot_pink", "indian_red", "indigo", "ivory", "khaki", "lavender", "lavender_blush", "lawn_green", "lemon_chiffon", "light_blue", "light_coral", "light_cyan", "light_goldenrod_yellow", "light_gray", "light_green", "light_pink", "light_salmon", "light_sea_green", "light_sky_blue", "light_slate_gray", "light_steel_blue", "light_yellow", "lime", "lime_green", "linen", "magenta", "maroon", "medium_aquamarine", "medium_blue", "medium_orchid", "medium_purple", "medium_sea_green", "medium_slate_blue", "medium_spring_green", "medium_turquoise", "medium_violet_red", "midnight_blue", "mint_cream", "misty_rose", "moccasin", "navajo_white", "navy", "old_lace", "olive", "olive_drab", "orange", "orange_red", "orchid", "pale_goldenrod", "pale_green", "pale_turquoise", "pale_violet_red", "papaya_whip", "peach_puff", "peru", "pink", "plum", "powder_blue", "purple", "red", "rosy_brown", "royal_blue", "saddle_brown", "salmon", "sandy_brown", "sea_green", "sea_shell", "sienna", "silver", "sky_blue", "slate_blue", "slate_gray", "snow", "spring_green", "steel_blue", "swinburne_red", "tan", "teal", "thistle", "tomato", "transparent", "turquoise", "violet", "wheat", "white", "white_smoke", "yellow", "yellow_green"];
 
+function getAllFiles(dir, allFilesList = []) {
+  try {
+    const files = fs.readdirSync(dir);
+    files.map(file => {
+      const name = dir + '/' + file;
+      if (fs.statSync(name).isDirectory()) { // check if subdirectory is present
+        getAllFiles(name, allFilesList);     // do recursive execution for subdirectory
+      } else {
+        allFilesList.push(file);             // push filename into the array
+      }
+    })
+  } catch (err) {
+    console.error(kleur.yellow(`Warning: Unable to access directory ${dir}`), err);
+  }
+  return allFilesList;
+}
+
+function getAllFinishedExamples() {
+  var apiJsonData;
+  try{
+    var apiData = fs.readFileSync(`${__dirname}/api.json`);
+    apiJsonData = JSON.parse(apiData);
+  } catch (error) {
+    console.error(kluer.red("Error occurred when trying to parse API Json data: ", error));
+  }
+
+  const categories = []
+  for (const categoryKey in apiJsonData){
+    if (categoryKey != "types"){
+      categories.push(categoryKey);
+    }
+  }
+
+  const allExamples = []; 
+
+  categories.forEach((categoryKey) => {
+    //let categoryFilePath = './public/usage-examples/' + categoryKey;
+    const categoryFilePath = path.join(path.dirname(__dirname), "public", "usage-examples", categoryKey);
+    const categoryFiles = getAllFiles(categoryFilePath);
+    
+    // Filter for .txt files
+    const txtFiles = categoryFiles.filter(file => file.endsWith('.txt'));
+    
+    // Extract the portion before the first '-'
+    if (txtFiles.length > 0)
+    {
+      txtFiles.forEach((file) => {
+        const filename = file.split('-')[0];
+        allExamples.push(filename);
+      });
+    }
+  });
+
+  return allExamples;
+}
+
 function Mappings(jsonData) {
   //generate mappings from API
   for (const categoryKey in jsonData) {
@@ -104,11 +160,13 @@ fs.readFile(`${__dirname}/api.json`, "utf8", async (err, data) => {
     console.log(`Generating MDX files for components`);
 
     const jsonColors = getColorData();
-
+    const usageExamples = getAllFinishedExamples();
+    console.log(usageExamples);
 
     // Please select an option: "animations, audio, camera, color, database, geometry, graphics, input, json, networking, physics, resource_bundles, resources, social, sprites, terminal, timers, types, utilities, windows"
     for (const categoryKey in jsonData) {
       const category = jsonData[categoryKey];
+      console.log(categoryKey);
       let input = categoryKey;
       const categoryFunctions = category.functions;
       let mdxContent = "";
@@ -157,6 +215,7 @@ fs.readFile(`${__dirname}/api.json`, "utf8", async (err, data) => {
         functionGroups[functionName].push(func);
       });
 
+
       for (const functionName in functionGroups) {
         const overloads = functionGroups[functionName];
         const isOverloaded = overloads.length > 1;
@@ -169,9 +228,15 @@ fs.readFile(`${__dirname}/api.json`, "utf8", async (err, data) => {
             .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
             .join(" ");
           const formattedLink = formattedFunctionName.toLowerCase().replace(/\s+/g, "-");
-
+        
           const formattedGroupLink = `${formattedLink}`;
           mdxContent += `\n### [${formattedFunctionName}](#${formattedGroupLink})\n\n`;
+          usageExamples.forEach((example) => {
+            if (functionName == example){
+              formattedUsageLink = functionName.replace(/_/g, "-");
+              mdxContent += `\n***[Usage Example Availabe](/usage-examples/${categoryKey}/#${formattedUsageLink})***\n`
+            }
+          }) 
           mdxContent += ":::note\n\n";
           mdxContent += "This function is overloaded. The following versions exist:\n\n";
 
@@ -237,8 +302,15 @@ fs.readFile(`${__dirname}/api.json`, "utf8", async (err, data) => {
             mdxContent += ` <div class='color-box' style="background:rgba${rgbValues}"></div>`
           }
           else {
-
             mdxContent += `${formattedName}`;
+            usageExamples.forEach((example) => {
+              if (!isOverloaded){
+                if (functionName == example){
+                  formattedUsageLink = functionName.replace(/_/g, "-");
+                  mdxContent += `\n***[Usage Example Availabe](/usage-examples/${categoryKey}/#${formattedUsageLink})***\n`
+                }
+              }
+            }) 
           }
 
           mdxContent += "\n\n";
@@ -362,6 +434,7 @@ fs.readFile(`${__dirname}/api.json`, "utf8", async (err, data) => {
                 .join(" ");
               const formattedLink = normalName.toLowerCase().replace(/\s+/g, "-");
               const link = `[\`${normalName}\`](/api/${input}/#${formattedLink})`
+
               description = description.replace(new RegExp(`\`\\b${names}\\b\``, "g"), link);
             }
 
