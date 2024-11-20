048a829b13aee32865ca7d3a9b65bd92946e189c
Jinx-means-jinx <www.2927196163@qq.com>
2024-11-20 23:17:31 +1100

    add usage example draw-ellipse

---
+++ public/usage-examples/graphics/draw_ellipse/draw_ellipse-1-radiating-wave.cpp
@@ -0,0 +1,25 @@
+#include "splashkit.h"
+
+int main()
+{
+    open_window("Draw Ellipse", 800, 600);
+    clear_screen();
+
+    for (int i = 0; i < 30; i++)
+    {
+        int width = 600 - i * 20;
+        int height = 400 - i * 10;
+        int x = 100 + i * 10;
+        int y = 100 + i * 5;
+
+        color randomColor = rgb_color(rand() % 255, rand() % 255, rand() % 255);
+
+        draw_ellipse(randomColor, x, y, width, height);
+    }
+
+    refresh_screen();
+    delay(4000);
+    close_all_windows();
+
+    return 0;
+}
