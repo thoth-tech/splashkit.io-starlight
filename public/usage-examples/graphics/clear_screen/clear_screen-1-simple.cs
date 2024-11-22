70c0b9366ee53bc293e15ae87cc94a6867c4b419
rmachu <rachelalonzochu@gmail.com>
2024-11-22 14:37:47 +1100

    fix pull request files

--- public/usage-examples/graphics/clear_screen/clear_screen-1-simple.cs
+++ public/usage-examples/graphics/clear_screen/clear_screen-1-simple.cs
@@ -1,12 +1,9 @@
 using static SplashKitSDK.SplashKit;
 
-//Open new window with title and dimensions
 OpenWindow("Blue Background", 800, 600);
 
-//Use Clear Screen to change the background color to blue 
 ClearScreen(ColorBlue());
 RefreshScreen(60);
 Delay(4000);
 
-//Close loaded windows   
 CloseAllWindows();
