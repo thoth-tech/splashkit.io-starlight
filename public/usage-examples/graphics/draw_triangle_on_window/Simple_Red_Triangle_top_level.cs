1e2e8adc71c3e3aae32452f87f3d325adc2b9bdb
huyhuynh2319 <huyhuynh2319@gmail.com>
2024-11-24 23:20:30 +1100

    Delete public/usage-examples/graphics/draw_triangle_on_window/Simple_Red_Triangle_top_level.cs

--- public/usage-examples/graphics/draw_triangle_on_window/Simple_Red_Triangle_top_level.cs
+++
@@ -1,9 +0,0 @@
-﻿using static SplashKitSDK.SplashKit;
-
-OpenWindow("Simple Red Triangle", 800, 600);
-{
-    ProcessEvents();
-    ClearScreen(Color.White);
-    FillTriangle(Color.Red, 150, 50, 250, 250, 50, 250);
-
-}
