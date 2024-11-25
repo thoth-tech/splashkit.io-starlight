bea8eeed365b0d8f5b146bd223b832116937af90
huyhuynh2319 <huyhuynh2319@gmail.com>
2024-11-25 15:01:58 +1100

    sprite_set_y

---
+++ public/usage-examples/sprites/sprite_set_y/sprite_set_y-1-simple-oop.cs
@@ -0,0 +1,25 @@
+using SplashKitSDK;
+
+namespace Program
+{
+    public class Program
+    {
+        public static void Main()
+        {
+         
+            Window myWindow = SplashKit.OpenWindow("sprite_set_y", 600, 600);
+            
+            Bitmap playerBitmap = SplashKit.LoadBitmap("player", "player-run.png");
+            
+            Sprite playerSprite = SplashKit.CreateSprite(playerBitmap);
+
+           
+            SplashKit.SpriteSetY(playerSprite, 300);
+            SplashKit.ClearScreen(Color.Black);
+            SplashKit.DrawSprite(playerSprite);
+            SplashKit.RefreshScreen();
+            SplashKit.Delay(5000);
+            SplashKit.CloseWindow(myWindow);
+    }
+  }
+}
---
+++ public/usage-examples/sprites/sprite_set_y/sprite_set_y-1-simple.py
@@ -0,0 +1,17 @@
+from splashkit import *
+
+
+my_window = open_window("sprite_set_y", 600, 600)
+
+player_bitmap = load_bitmap("player", "player-run.png")
+
+player_sprite = create_sprite(player_bitmap)
+
+sprite_set_y(player_sprite, 300)
+
+
+clear_screen(color_black())
+draw_sprite(player_sprite)
+refresh_screen()
+delay(5000)
+close_window(my_window)
