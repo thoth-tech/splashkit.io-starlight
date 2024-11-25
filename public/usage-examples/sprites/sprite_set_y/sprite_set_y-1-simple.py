4a8a39b8fe2adc5fdb06444ca2650b6595b3b39c
huyhuynh2319 <huyhuynh2319@gmail.com>
2024-11-25 16:27:52 +1100

    Update sprite_set_y-1-simple.py

--- public/usage-examples/sprites/sprite_set_y/sprite_set_y-1-simple.py
+++ public/usage-examples/sprites/sprite_set_y/sprite_set_y-1-simple.py
@@ -9,16 +9,13 @@ player_sprite = create_sprite(player_bitmap)
 
 sprite_set_y(player_sprite, 300)
 
-<<<<<<< Updated upstream
 
-=======
->>>>>>> Stashed changes
+
+
 clear_screen(color_black())
 draw_sprite(player_sprite)
 refresh_screen()
 delay(5000)
-<<<<<<< Updated upstream
+
 close_window()
-=======
-close_window()
->>>>>>> Stashed changes
+
