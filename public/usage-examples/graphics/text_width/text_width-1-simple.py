1d4e111d5979c391f287b5a1b16cab1d45feee86
Jinx-means-jinx <www.2927196163@qq.com>
2024-11-25 13:41:45 +1100

    usage example text-width

---
+++ public/usage-examples/graphics/text_width/text_width-1-simple.py
@@ -0,0 +1,25 @@
+from splashkit import *
+
+# Open a window
+open_window("Text Width", 800, 600)
+
+# Clear the screen to a black background
+clear_screen(color_white())
+
+# Text to display
+text = "Text Width!"
+
+# Calculate the text width with Normal font '0' and size 16
+text_width = text_width(text, 0, 16)
+
+# Calculate the x and y position to make the text in the centre of the window
+x_position = (800 - text_width) // 2
+y_position = 600 // 2
+
+# Display the text in the centre of the window
+draw_text(text, color_black(), 0, 16, x_position, y_position)
+
+
+refresh_screen()
+delay(4000)
+close_all_windows()
