71a0896f8d69389dfd38539f18d02b960d85b2ff
omckeon <ordmckeon@gmail.com>
2024-12-02 12:15:31 +0000

    Update formatting and fix Window name in C# OOP code and python code

--- public/usage-examples/graphics/fill_triangle/fill_triangle-1-simple-oop.cs
+++ public/usage-examples/graphics/fill_triangle/fill_triangle-1-simple-oop.cs
@@ -1,19 +1,19 @@
 using SplashKitSDK;
 
-namespace Program
+namespace FillTriangleExample
 {
-  public class Program
-  {
-    public static void Main()
+    public class Program
     {
-      SplashKit.OpenWindow("Fill Triangle", 800, 600);
-      SplashKit.ClearScreen();
+        public static void Main()
+        {
+            SplashKit.OpenWindow("Fill Triangle Example", 800, 600);
 
-      SplashKit.FillTriangle(Color.Red, 100, 100, 200, 200, 300, 100);
-      SplashKit.RefreshScreen();
+            SplashKit.ClearScreen();
+            SplashKit.FillTriangle(Color.Red, 100, 100, 200, 200, 300, 100);
+            SplashKit.RefreshScreen();
 
-      SplashKit.Delay(5000);
-      SplashKit.CloseAllWindows();
+            SplashKit.Delay(5000);
+            SplashKit.CloseAllWindows();
+        }
     }
-  }
 }
--- public/usage-examples/graphics/fill_triangle/fill_triangle-1-simple.py
+++ public/usage-examples/graphics/fill_triangle/fill_triangle-1-simple.py
@@ -1,7 +1,8 @@
 from splashkit import *
 
-open_window("Fill triangle", 800, 600)
+open_window("Fill Triangle Example", 800, 600)
 
+clear_screen_to_white()
 fill_triangle(color_red(), 100, 100, 200, 200, 300, 100)
 refresh_screen()
 
