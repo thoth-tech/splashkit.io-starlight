b0e36e078e98a19aa90bd80a58527c55c71b4b8b
omckeon <ordmckeon@gmail.com>
2024-12-02 11:28:13 +0000

    Minor formatting updates

--- public/usage-examples/graphics/clear_screen/clear_screen-1-simple-oop.cs
+++ public/usage-examples/graphics/clear_screen/clear_screen-1-simple-oop.cs
@@ -1,22 +1,19 @@
 ﻿using SplashKitSDK;
 
-namespace ClearScreen 
+namespace ClearScreenExample
 {
-    public class Program 
+    public class Program
     {
         public static void Main()
         {
-            //Open new window with title and dimensions
             SplashKit.OpenWindow("Blue Background", 800, 600);
 
-            //Use Clear Screen to change the background color to blue 
+            //Use Clear Screen to change the background color to blue
             SplashKit.ClearScreen(Color.Blue);
+
             SplashKit.RefreshScreen(60);
             SplashKit.Delay(4000);
-            
-            //Close loaded windows 
             SplashKit.CloseAllWindows();
         }
     }
-
 }
--- public/usage-examples/graphics/clear_screen/clear_screen-1-simple.cpp
+++ public/usage-examples/graphics/clear_screen/clear_screen-1-simple.cpp
@@ -2,15 +2,13 @@
 
 int main()
 {
-    //Open new window with title and dimensions
     open_window("Blue Background", 800, 600);
 
     //Use Clear Screen to change the background color to blue 
     clear_screen(COLOR_BLUE);
+
     refresh_screen(60);
     delay(4000);
-    
-    //Close loaded windows   
     close_all_windows();
 
     return 0;
