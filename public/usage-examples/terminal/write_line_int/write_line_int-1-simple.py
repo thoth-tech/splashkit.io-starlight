cc1d680ab908343eed8798a680b1db272c8b1558
SimonRhook <rhooki13@gmail.com>
2024-12-02 21:49:58 +0000

    updated python to use correct function

--- public/usage-examples/terminal/write_line_int/write_line_int-1-simple.py
+++ public/usage-examples/terminal/write_line_int/write_line_int-1-simple.py
@@ -1,22 +1,22 @@
 from splashkit import *
 
 # Example 1: Print single integer
-write_line(1)
-write_line(2)
-write_line(3)
-write_line(-1)
-write_line(-2)
-write_line(-3)
+write_line_int(1)
+write_line_int(2)
+write_line_int(3)
+write_line_int(-1)
+write_line_int(-2)
+write_line_int(-3)
 
 # Example 2: Print multi-digit integer
-write_line(12345)
-write_line(953221311)
-write_line(-165746)
+write_line_int(12345)
+write_line_int(953221311)
+write_line_int(-165746)
 
 # Example 3: Print integer after calculation
 a = 222-111
 b = 10 * 12
 c = 100 / 5
-write_line(a - b)
-write_line(b)
-write_line(c)
+write_line_int(a - b)
+write_line_int(b)
+write_line_int(int(c))
