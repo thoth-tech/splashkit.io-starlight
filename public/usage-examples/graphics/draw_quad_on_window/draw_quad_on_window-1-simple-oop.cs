5baf6f514af96927a22e1c63c5b6fcc861dbd294
SimonRhook <rhooki13@gmail.com>
2024-12-03 03:15:26 +0000

    Updated OOP, reformatted code, updated description

--- public/usage-examples/graphics/draw_quad_on_window/draw_quad_on_window-1-simple-oop.cs
+++ public/usage-examples/graphics/draw_quad_on_window/draw_quad_on_window-1-simple-oop.cs
@@ -6,28 +6,25 @@ namespace DrawQuadOnWindow
     {
         public static void Main()
         {
-            // Initialise quads with x1,y1,...,x4,y4
-            Quad q1 = SplashKit.QuadFrom(400,200,300,300,300,0,200,200);
-            Quad q2 = SplashKit.QuadFrom(400,210,310, 300,600,300,400,390);
-            Quad q3 = SplashKit.QuadFrom(200,400,300, 300,300,600,400,400);
-            Quad q4 = SplashKit.QuadFrom(200,390,290, 300,0,300,200,210);
+            // Initialise quads with x1, y1,..., x4, y4
+            Quad Q1 = SplashKit.QuadFrom(400, 200, 300, 300, 300, 0, 200, 200);
+            Quad Q2 = SplashKit.QuadFrom(400, 210, 310, 300, 600, 300, 400, 390);
+            Quad Q3 = SplashKit.QuadFrom(200, 400, 300, 300, 300, 600, 400, 400);
+            Quad Q4 = SplashKit.QuadFrom(200, 390, 290, 300, 0, 300, 200, 210);
 
-            // Create Window
-
-            Window window1 = SplashKit.OpenWindow("Draw Quad On Window 1", 600, 600);
-            Window window2 = SplashKit.OpenWindow("Draw Quad On Window 2", 600, 600);
+            // Create Windows
+            Window Window1 = SplashKit.OpenWindow("Diamonds On Window 1", 600, 600);
+            Window Window2 = SplashKit.OpenWindow("Diamonds On Window 2", 600, 600);
             SplashKit.ClearScreen(SplashKit.ColorWhite());
 
-
-            SplashKit.DrawQuadOnWindow(window1, SplashKit.ColorBlack(), q1);
-            SplashKit.DrawQuadOnWindow(window1, SplashKit.ColorGreen(), q2);
-            SplashKit.DrawQuadOnWindow(window2, SplashKit.ColorRed(), q3);
-            SplashKit.DrawQuadOnWindow(window2, SplashKit.ColorBlue(), q4);
-            
+            Window1.DrawQuad(Color.Black, Q1);
+            Window1.DrawQuad(Color.Green, Q2);
+            Window2.DrawQuad(Color.Red, Q3);
+            Window2.DrawQuad(Color.Blue, Q4);
 
             SplashKit.RefreshScreen();
             SplashKit.Delay(5000);
             SplashKit.CloseAllWindows();
-        }    
+        }
     }
 }
--- public/usage-examples/graphics/draw_quad_on_window/draw_quad_on_window-1-simple-top-level.cs
+++ public/usage-examples/graphics/draw_quad_on_window/draw_quad_on_window-1-simple-top-level.cs
@@ -1,23 +1,22 @@
 using SplashKitSDK;
 using static SplashKitSDK.SplashKit;
 
+// Initialise quads with x1, y1, ..., x4, y4
+Quad Q1 = QuadFrom(400, 200, 300, 300, 300, 0, 200, 200);
+Quad Q2 = QuadFrom(400, 210, 310, 300, 600, 300, 400, 390);
+Quad Q3 = QuadFrom(200, 400, 300, 300, 300, 600, 400, 400);
+Quad Q4 = QuadFrom(200, 390, 290, 300, 0, 300, 200, 210);
 
-//Initialise quads with x1,y1,...,x4,y4
-Quad q1 = QuadFrom(400,200,300,300,300,0,200,200);   
-Quad q2 = QuadFrom(400,210,310, 300,600,300,400,390);
-Quad q3 = QuadFrom(200,400,300, 300,300,600,400,400);
-Quad q4 = QuadFrom(200,390,290, 300,0,300,200,210);   
-
-// Create Window
-Window window1 = OpenWindow("Draw Quad On Window 1", 600, 600);
-Window window2 = OpenWindow("Draw Quad On Window 2", 600, 600);
+// Create Windows
+Window Window1 = OpenWindow("Diamonds On Window 1", 600, 600);
+Window Window2 = OpenWindow("Diamonds On Window 2", 600, 600);
 ClearScreen(ColorWhite());
 
-
-DrawQuadOnWindow(window1, ColorBlack(), q1);
-DrawQuadOnWindow(window1, ColorGreen(), q2);
-DrawQuadOnWindow(window2, ColorRed(), q3);
-DrawQuadOnWindow(window2, ColorBlue(), q4);
+// Draw quads on each window
+DrawQuadOnWindow(Window1, ColorBlack(), Q1);
+DrawQuadOnWindow(Window1, ColorGreen(), Q2);
+DrawQuadOnWindow(Window2, ColorRed(), Q3);
+DrawQuadOnWindow(Window2, ColorBlue(), Q4);
 
 RefreshScreen();
 Delay(5000);
