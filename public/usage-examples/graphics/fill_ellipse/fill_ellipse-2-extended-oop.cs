0b4797c16ac5cb46f4e424ab6cab3960e64f49ef
EthanRin <sovanpanhaarinn@gmail.com>
2024-12-11 16:14:47 +1100

    updated the color to OOP version and the spelling mistake in text file

--- public/usage-examples/graphics/fill_ellipse/fill_ellipse-2-extended-oop.cs
+++ public/usage-examples/graphics/fill_ellipse/fill_ellipse-2-extended-oop.cs
@@ -1,18 +1,18 @@
 using SplashKitSDK;
 
-namespace Program
+namespace CloverDrawing
 {
-    public class Program
+    public class CloverDrawing
     {
         public static void Main()
         {
             SplashKit.OpenWindow("Clover Drawing with Fill Ellipse", 1000, 600);
 
             SplashKit.ClearScreen();
-            SplashKit.FillEllipse(SplashKit.ColorGreen(), 150, 225, 400, 200);
-            SplashKit.FillEllipse(SplashKit.ColorGreen(), 375, 0, 200, 400);
-            SplashKit.FillEllipse(SplashKit.ColorGreen(), 400, 225, 400, 200);
-            SplashKit.FillRectangle(SplashKit.ColorBrown(), 470, 400, 10, 150);
+            SplashKit.FillEllipse(Color.Green, 150, 225, 400, 200);
+            SplashKit.FillEllipse(Color.Green, 375, 0, 200, 400);
+            SplashKit.FillEllipse(Color.Green, 400, 225, 400, 200);
+            SplashKit.FillRectangle(Color.Brown, 470, 400, 10, 150);
             SplashKit.RefreshScreen();
 
             SplashKit.Delay(4000);
