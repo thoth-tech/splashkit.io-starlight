792a56524f3935cf872668dee5fe7d499db048e2
omckeon <ordmckeon@gmail.com>
2024-12-16 07:11:17 +0000

    Formatting

--- public/usage-examples/graphics/fill_quad/fill_quad-1-simple-oop.cs
+++ public/usage-examples/graphics/fill_quad/fill_quad-1-simple-oop.cs
@@ -1,22 +1,21 @@
 using SplashKitSDK;
 
-namespace FillQuad
+namespace FillQuadExample
 {
     public class Program
     {
         public static void Main()
         {
-            // Initialise quads with x1, y1, ..., x4, y4
+            // Create 4 diamond shapes using quads
             Quad Q1 = SplashKit.QuadFrom(400, 200, 300, 300, 300, 0, 200, 200);
             Quad Q2 = SplashKit.QuadFrom(400, 210, 310, 300, 600, 300, 400, 390);
             Quad Q3 = SplashKit.QuadFrom(200, 400, 300, 300, 300, 600, 400, 400);
             Quad Q4 = SplashKit.QuadFrom(200, 390, 290, 300, 0, 300, 200, 210);
 
-            // Create Window
             SplashKit.OpenWindow("Coloured Star", 600, 600);
             SplashKit.ClearScreen(Color.White);
 
-            // Fill the quads with colors
+            // Draw filled-in quads
             SplashKit.FillQuad(Color.Black, Q1);
             SplashKit.FillQuad(Color.Green, Q2);
             SplashKit.FillQuad(Color.Red, Q3);
