6115f080ca05b4de317e84cb55c634ad04059ad6
omckeon <ordmckeon@gmail.com>
2024-12-16 07:35:31 +0000

    Formatting

--- public/usage-examples/graphics/fill_quad_on_window/fill_quad_on_window-1-simple-top-level.cs
+++ public/usage-examples/graphics/fill_quad_on_window/fill_quad_on_window-1-simple-top-level.cs
@@ -1,21 +1,29 @@
 using SplashKitSDK;
 using static SplashKitSDK.SplashKit;
 
-// Initialise quads with x1, y1,..., x4, y4
-Quad Q1 = QuadFrom(400, 200, 300, 300, 300, 0, 200, 200);   
-Quad Q2 = QuadFrom(400, 210, 310, 300, 600, 300, 400, 390);
-Quad Q3 = QuadFrom(200, 400, 300, 300, 300, 600, 400, 400);
-Quad Q4 = QuadFrom(200, 390, 290, 300, 0, 300, 200, 210);   
+// Create diamond shaped quads
+Quad q1 = QuadFrom(400, 200, 300, 300, 300, 0, 200, 200);
+Quad q2 = QuadFrom(400, 210, 310, 300, 600, 300, 400, 390);
+Quad q3 = QuadFrom(200, 400, 300, 300, 300, 600, 400, 400);
+Quad q4 = QuadFrom(200, 390, 290, 300, 0, 300, 200, 210);
 
-// Create Window
+// Create two Windows
 Window window1 = OpenWindow("Filled Diamond On Window 1", 600, 600);
 Window window2 = OpenWindow("Filled Diamond On Window 2", 600, 600);
+
+// Move windows to see both side by side
+MoveWindowTo(window1, 0, 0);
+MoveWindowTo(window2, WindowWidth(window1), 0);
+
 ClearScreen(ColorWhite());
 
-FillQuadOnWindow(window1, ColorBlack(), Q1);
-FillQuadOnWindow(window1, ColorGreen(), Q2);
-FillQuadOnWindow(window2, ColorRed(), Q3);
-FillQuadOnWindow(window2, ColorBlue(), Q4);
+// Draw the first and second quad on first window
+FillQuadOnWindow(window1, ColorBlack(), q1);
+FillQuadOnWindow(window1, ColorGreen(), q2);
+
+// Draw the third and fourth quad on second window
+FillQuadOnWindow(window2, ColorRed(),  q3);
+FillQuadOnWindow(window2, ColorBlue(), q4);
 
 RefreshScreen();
 Delay(5000);
