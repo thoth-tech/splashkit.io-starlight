59f5579a6e9cc324979e4e39dd73ce48365d78ff
omckeon <ordmckeon@gmail.com>
2024-12-17 06:43:41 +0000

    Update Graphics guide with more instructions

--- public/beyond-splashkit/code-files/graphics/0-getting-started-with-graphics/open_window-no-splashkit.cpp
+++ public/beyond-splashkit/code-files/graphics/0-getting-started-with-graphics/open_window-no-splashkit.cpp
@@ -1,38 +1,46 @@
-#include <iostream>
-#include <SDL2/SDL.h>
 
-int main(int argv, char** args)    
+#include <iostream>
+#ifdef __APPLE__
+#include <SDL.h>
+#else
+#include <SDL2/SDL.h>
+#endif
+
+int main(int argv, char **args)
 {
-    // Open window without SplashKit.
+    // Opening a Window without SplashKit
 
     // Declare Variables
-    SDL_Window* window = nullptr;
+    SDL_Window *window = nullptr;
 
     // Check for successful initialisation
-    if(SDL_Init(SDL_INIT_VIDEO) < 0){
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
         std::cout << "SDL could not be initialized: " << SDL_GetError();
         exit(1);
     }
 
     // Create Window
     window = SDL_CreateWindow(
-        "No SK Window",                  
-        SDL_WINDOWPOS_CENTERED,            
-        SDL_WINDOWPOS_CENTERED,   
-        800,                               
-        600,                               
-        SDL_WINDOW_OPENGL                  
-    );
+        "No SK Window: OpenWindow",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        800,
+        600,
+        SDL_WINDOW_OPENGL);
+
     // Error handling for window
-    if (window == NULL) {
+    if (window == NULL)
+    {
         SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window: %s\n", SDL_GetError());
         exit(1);
-    }   
+    }
 
     // Hold window 4 seconds
     SDL_Delay(4000);
+
     // Cleanup and free memory
     SDL_DestroyWindow(window);
-    
+
     return 0;
 }
--- public/beyond-splashkit/code-files/graphics/0-getting-started-with-graphics/quit_requested-no-splashkit.cpp
+++ public/beyond-splashkit/code-files/graphics/0-getting-started-with-graphics/quit_requested-no-splashkit.cpp
@@ -1,50 +1,56 @@
 #include <iostream>
+#ifdef __APPLE__
+#include <SDL.h>
+#else
 #include <SDL2/SDL.h>
+#endif
 
-
-
-SDL_Window* sdl_open_window()
+SDL_Window *sdl_open_window(const char *window_title, const int width, const int height)
 {
     // This function demonstrates how we can open window without SplashKit.
 
     // Declare Variables
-    SDL_Window* window = nullptr;
+    SDL_Window *window = nullptr;
 
     // Check for successful initialisation
-    if(SDL_Init(SDL_INIT_VIDEO) < 0){
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
         std::cout << "SDL could not be initialized: " << SDL_GetError();
         exit(1);
     }
 
     // Create Window
     window = SDL_CreateWindow(
-        "No SK Window",                  
-        SDL_WINDOWPOS_CENTERED,            
-        SDL_WINDOWPOS_CENTERED,   
-        800,                               
-        600,                               
-        SDL_WINDOW_OPENGL                  
-    );
+        window_title,
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        width,
+        height,
+        SDL_WINDOW_OPENGL);
+
     // Error handling for window
-    if (window == NULL) {
+    if (window == NULL)
+    {
         SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window: %s\n", SDL_GetError());
         exit(1);
-    }   
+    }
 
     return window;
 }
 
-int main(int argv, char** args)
+int main(int argv, char **args)
 {
-    
-    SDL_Window* window = sdl_open_window();
-    
+    SDL_Window *window = sdl_open_window("No SK Window: QuitRequested", 800, 600);
+
     // Hang window until quit
     SDL_Event event;
-    while (event.type != SDL_QUIT) {
+    while (event.type != SDL_QUIT)
+    {
         SDL_PollEvent(&event);
     }
+
     // Cleanup and free memory
     SDL_DestroyWindow(window);
+
     return 0;
 }
