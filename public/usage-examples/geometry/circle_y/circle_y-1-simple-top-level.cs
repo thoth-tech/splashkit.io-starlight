491b2349ddf2949056f165e23799f7e60f767119
Jinx-means-jinx <113697592+Jinx-means-jinx@users.noreply.github.com>
2025-01-04 16:56:23 +1100

    Update circle_y-1-simple-top-level.cs

--- public/usage-examples/geometry/circle_y/circle_y-1-simple-top-level.cs
+++ public/usage-examples/geometry/circle_y/circle_y-1-simple-top-level.cs
@@ -4,26 +4,27 @@ using static SplashKitSDK.SplashKit;
 OpenWindow("Circle Y", 800, 600);
 ClearScreen();
 
-//Set position for the circle
+// Set position for the circle
 double x_position = 400;
-//Give random  y_position value bewteen 200 - 400
+// Give random  y_position value bewteen 200 - 400
 double y_position = Rnd(200) + 200;
 
-//Create a circle A at the position (x_position, y_position)
+// Create a circle A at the position (x_position, y_position)
 Circle A = CircleAt(x_position, y_position, 200);
-//Find the y position of the circle
+// Find the y position of the circle
 double circleY = CircleY(A);
 
-//Draw the Circle
+// Draw the Circle
 DrawCircle(Color.Red, x_position, circleY, 200);
 
+// Draw a line to show the circle Y coordinate
 DrawLine(Color.Black, 0, circleY, 800, circleY);
 
 string text = "Circle Y: " + circleY.ToString();
 // Print result on window
 DrawText(text, Color.Black, 100, 100);
 
-//Draw 10 circle with radient 50 and the same circle y coordinate
+// Draw 10 circle with radient 50 and the same circle y coordinate
 for (int i = 0; i < 10; i++)
 {
     int x = i * 60 + 100;
