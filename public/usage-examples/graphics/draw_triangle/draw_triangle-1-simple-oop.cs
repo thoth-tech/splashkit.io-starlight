fc8d1b00b2fd0202d1efb975919a7c4abcf20cc6
Jinx-means-jinx <www.2927196163@qq.com>
2025-01-06 17:36:53 +1100

    change base on the PR

--- public/usage-examples/graphics/draw_triangle/draw_triangle-1-simple-oop.cs
+++ public/usage-examples/graphics/draw_triangle/draw_triangle-1-simple-oop.cs
@@ -9,26 +9,30 @@ namespace DrawTriangle
         SplashKit.OpenWindow("Draw Triangle", 800, 600);
         SplashKit.ClearScreen();
 
-        Random random = new Random();
         for (int i = 0; i < 10; i++)
-        {
-            int x1 = random.Next(800);
-            int y1 = random.Next(600);
-            int x2 = random.Next(800);
-            int y2 = random.Next(600);
-            int x3 = random.Next(800);
-            int y3 = random.Next(600);
+        {   
+            // Random point 1 for the trangle (x1,y1)
+            int x1 = SplashKit.Rnd(800);
+            int y1 = SplashKit.Rnd(600);
+
+            // Random point 2 for the trangle (x2,y2)
+            int x2 = SplashKit.Rnd(800);
+            int y2 = SplashKit.Rnd(600);
+
+            // Random point 3 for the trangle (x3,y3)
+            int x3 = SplashKit.Rnd(800);
+            int y3 = SplashKit.Rnd(600);
 
             Color randomColor = SplashKit.RGBColor(
                 SplashKit.Rnd(255), SplashKit.Rnd(255), SplashKit.Rnd(255)
             );
 
+            // Draw trangle base on the three random points
             SplashKit.DrawTriangle(randomColor, x1, y1, x2, y2, x3, y3);
         }
 
         SplashKit.RefreshScreen();
         SplashKit.Delay(4000);
-
         SplashKit.CloseAllWindows();
         }
     }
