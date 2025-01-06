c8510357b441d04941a0f6d3fe50adbf5cbee4dd
Jinx-means-jinx <www.2927196163@qq.com>
2025-01-06 17:20:46 +1100

    change base on the PR

--- public/usage-examples/graphics/draw_circle_on_window/draw_circle_on_window-1-simple-top-level.cs
+++ public/usage-examples/graphics/draw_circle_on_window/draw_circle_on_window-1-simple-top-level.cs
@@ -5,20 +5,19 @@ Window myWindow = OpenWindow("Draw Circle on Window", 800, 600);
 
 ClearScreen();
 
-Random random = new Random();
 for (int i = 0; i < 50; i++)
 {
     // Set random x position to 1 - 800
-    double x = random.Next(800);
+    double x = Rnd(800);
 
     // Set random y position to 1 - 600
-    double y = random.Next(600);
+    double y = Rnd(600);
 
     // Set random radius to 1 - 50
-    double radius = random.Next(50);
+    double radius = Rnd(50);
 
     Color randomColor = RGBColor(
-        random.Next(255), random.Next(255), random.Next(255)
+        Rnd(255), Rnd(255), Rnd(255)
     );
 
     // Draw the circle base on the random data
