4f1354396b2c9b6e81736a0c9f28300b1db3705c
Jinx-means-jinx <www.2927196163@qq.com>
2025-01-12 17:23:50 +1100

    .

--- public/usage-examples/geometry/center_point/center_point-1-simple-oop.cs
+++ public/usage-examples/geometry/center_point/center_point-1-simple-oop.cs
@@ -1,38 +1,38 @@
 using SplashKitSDK;
 
-namespace CenterPoint
+namespace CenterPointExample
 {
     public class Program
     {
         public static void Main()
         {
-        SplashKit.OpenWindow("Center Point", 800, 600);
-        SplashKit.ClearScreen();
+            Window window = new Window("Center Point", 800, 600);
+            window.Clear(Color.White);
 
-        // Set position for the circle
-        double x_position = 400;
-        double y_position = 300;
+            // Set position for the circle
+            double x_position = 400;
+            double y_position = 300;
 
-        // Create A circle name "A"
-        Circle A = SplashKit.CircleAt(x_position, y_position, 200);
+            // Create A circle name "A"
+            Circle A = SplashKit.CircleAt(x_position, y_position, 200);
 
-        //Fill the Circle A
-        SplashKit.FillCircle(Color.Red, A);
+            //Fill the Circle A
+            SplashKit.FillCircle(Color.Red, A);
 
-        //Using the center point to fill a triangle to cut the circle
-        SplashKit.FillTriangle(Color.White, SplashKit.CenterPoint(A).X, SplashKit.CenterPoint(A).Y, 0, 300, 500, 0);
+            //Using the center point to fill a triangle to cut the circle
+            SplashKit.FillTriangle(Color.White, SplashKit.CenterPoint(A).X, SplashKit.CenterPoint(A).Y, 0, 300, 500, 0);
 
-        //Draw Point in the center of the circle
-        SplashKit.FillCircle(Color.Black, SplashKit.CenterPoint(A).X, SplashKit.CenterPoint(A).Y, 3);
-    
-        string text = "Center Point At: " + SplashKit.PointToString(SplashKit.CenterPoint(A));
-        
-        // Print result on window
-        SplashKit.DrawText(SplashKit.PointToString(SplashKit.CenterPoint(A)), Color.Black, x_position -20, y_position - 20);
+            //Draw Point in the center of the circle
+            SplashKit.FillCircle(Color.Black, SplashKit.CenterPoint(A).X, SplashKit.CenterPoint(A).Y, 3);
 
-        SplashKit.RefreshScreen();
-        SplashKit.Delay(4000);
-        SplashKit.CloseAllWindows();
+            string text = "Center Point At: " + SplashKit.PointToString(SplashKit.CenterPoint(A));
+
+            // Print result on window
+            SplashKit.DrawText(SplashKit.PointToString(SplashKit.CenterPoint(A)), Color.Black, x_position - 20, y_position - 20);
+
+            window.Refresh();
+            SplashKit.Delay(4000);
+            window.Close();
         }
     }
 }
