09e48a685c88bad5147b824fffe3c68b5254829a
Jinx-means-jinx <www.2927196163@qq.com>
2025-01-12 17:19:30 +1100

    .

--- public/usage-examples/graphics/fill_rectangle/fill_rectangle-1-simple-oop.cs
+++ public/usage-examples/graphics/fill_rectangle/fill_rectangle-1-simple-oop.cs
@@ -1,24 +1,24 @@
 using SplashKitSDK;
 
-namespace FillRectangle
+namespace FillRectangleExample
 {
     public class Program
     {
         public static void Main()
-        {   
+        {
             //Create a window with title Fill rectangle, width 800, height 600.
-            SplashKit.OpenWindow("Fill Rectangle", 800, 600);
-            SplashKit.ClearScreen();
+            Window window = new Window("Fill Rectangle", 800, 600);
+            window.Clear(Color.White);
 
             //draw three rectangles with color green, yellow, and red with different position as (100,200), (300,200), (500,200) with same size.
             SplashKit.FillRectangle(SplashKit.ColorGreen(), 100, 200, 200, 100);
             SplashKit.FillRectangle(SplashKit.ColorYellow(), 300, 200, 200, 100);
             SplashKit.FillRectangle(SplashKit.ColorRed(), 500, 200, 200, 100);
 
-            SplashKit.RefreshScreen();
+            window.Refresh();
             SplashKit.Delay(4000);
-            SplashKit.CloseAllWindows();
-        }    
+            window.Close();
+        }
     }
 }
 
