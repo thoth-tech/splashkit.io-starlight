b321385ad407efec80ccb8717ec639c9ea642ce4
Jinx-means-jinx <www.2927196163@qq.com>
2025-01-12 17:18:14 +1100

    .

--- public/usage-examples/geometry/circle_at/circle_at-1-simple-oop.cs
+++ public/usage-examples/geometry/circle_at/circle_at-1-simple-oop.cs
@@ -1,31 +1,31 @@
 using SplashKitSDK;
 
-namespace CircleAt
+namespace CircleAtExample
 {
     public class Program
     {
         public static void Main()
         {
-        SplashKit.OpenWindow("Circle At", 800, 600);
-        SplashKit.ClearScreen();
+            Window window = new Window("Circle At", 800, 600);
+            window.Clear(Color.White);
 
-        // Set position for the circle
-        double x_position = 400;
-        double y_position = 300;
+            // Set position for the circle
+            double x_position = 400;
+            double y_position = 300;
 
-        // Create A circle name "A"
-        Circle A = SplashKit.CircleAt(x_position, y_position, 200);
+            // Create A circle name "A"
+            Circle A = SplashKit.CircleAt(x_position, y_position, 200);
 
-        //Draw the Circle
-        SplashKit.DrawCircle(Color.Red, A);
-    
-        string text = "Circle At: (400,300), Radius: 200";
-        // Print result on window
-        SplashKit.DrawText(text, Color.Black, "NORMAL_FONT", 20, 100, 100);
+            //Draw the Circle
+            SplashKit.DrawCircle(Color.Red, A);
 
-        SplashKit.RefreshScreen();
-        SplashKit.Delay(4000);
-        SplashKit.CloseAllWindows();
+            string text = "Circle At: (400,300), Radius: 200";
+            // Print result on window
+            SplashKit.DrawText(text, Color.Black, "NORMAL_FONT", 20, 100, 100);
+
+            window.Refresh();
+            SplashKit.Delay(4000);
+            window.Close();
         }
     }
 }
