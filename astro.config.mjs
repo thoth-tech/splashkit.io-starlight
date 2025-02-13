909b78c872b4714217c28808cbdebc7d577d7a3a
omckeon <ordmckeon@gmail.com>
2025-02-13 12:56:37 +0000

    Minor updates + testing secret variables in workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ import starlightDocSearch from '@astrojs/starlight-docsearch';
 import { loadEnv } from "vite";
 
 const { DOCSEARCH_API_ID } = loadEnv(process.env.DOCSEARCH_API_ID, process.cwd(), "");
-const { DOCSEARCH_API_SEARCH_KEY } = loadEnv(process.env.DOCSEARCH_API_SERACH_KEY, process.cwd(), "");
+const { DOCSEARCH_API_SEARCH_KEY } = loadEnv(process.env.DOCSEARCH_API_SEARCH_KEY, process.cwd(), "");
 const { DOCSEARCH_INDEX_NAME } = loadEnv(process.env.DOCSEARCH_INDEX_NAME, process.cwd(), "");
 
 if (!DOCSEARCH_API_ID || !DOCSEARCH_API_SEARCH_KEY || !DOCSEARCH_INDEX_NAME){
@@ -37,7 +37,7 @@ export default defineConfig({
         }),
         starlightDocSearch({
           appId: DOCSEARCH_API_ID,
-          apiKey: DOCSEARCH_API_SERACH_KEY,
+          apiKey: DOCSEARCH_API_SEARCH_KEY,
           indexName: DOCSEARCH_INDEX_NAME,
         }),
       ],
