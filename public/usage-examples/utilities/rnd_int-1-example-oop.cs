3baee06f8ce67a585e5309ffd088a4e193d00192
omckeon <ordmckeon@gmail.com>
2025-03-03 02:08:05 +0000

    Update rnd_int example for merging

--- public/usage-examples/utilities/rnd_int-1-example-oop.cs
+++ public/usage-examples/utilities/rnd_int-1-example-oop.cs
@@ -1,6 +1,6 @@
 ﻿using SplashKitSDK;
 
-namespace Program
+namespace RndExample
 {
     public class Program
     {
--- public/usage-examples/utilities/rnd_int-1-example.py
+++ public/usage-examples/utilities/rnd_int-1-example.py
@@ -7,5 +7,5 @@ delay(2000) # Delay for 2 seconds
 # Generate a random number up to the ubound
 random_number = rnd_int(1000)
 
-write_line(f"🎲 Your lucky number is: {random_number} 🎲")
+write_line(f"Your lucky number is: {random_number}")
 write_line("Feeling lucky? Maybe it's time to play the lottery!")
