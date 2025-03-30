2d8a22df26b62882203d8ef443ad6ac2f7bf957d
EthanRin <sovanpanhaarinn@gmail.com>
2025-03-30 17:31:38 +1100

    updated resources folder and python files

--- public/usage-examples/graphics/text_width_font_named-1-example.py
+++ public/usage-examples/graphics/text_width_font_named-1-example.py
@@ -3,12 +3,12 @@ from splashkit import *
 open_window("Text Width", 680, 200)
 clear_screen(color_white())
 
-load_font("LOTR", "lotr.TTF");
-set_font_style("LOTR", font_style.bold_font)
-draw_text("Ringbearer", color_black(), "LOTR", 100, 30, 20)
+load_font("LOTR", "lotr.TTF")
+set_font_style_name_as_string("LOTR", FontStyle.bold_font)
+draw_text_font_as_string("Ringbearer", color_black(), "LOTR", 100, 30, 20)
 
 width = text_width_font_named("Ringbearer", "LOTR", 100)
-draw_rectangle(color_black(), 30, 130, width, 10)
+fill_rectangle(color_black(), 30, 130, width, 10)
 refresh_screen()
 
 delay(5000)
