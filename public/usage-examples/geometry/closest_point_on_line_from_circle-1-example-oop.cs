52c3c58bf7744b3e9c2af704a63ce8f576183ae3
CATHoDE33 <94217306+CATHoDE33@users.noreply.github.com>
2025-04-11 00:35:33 +1000

    Add files via upload

---
+++ public/usage-examples/geometry/closest_point_on_line_from_circle-1-example-oop.cs
@@ -0,0 +1,36 @@
+﻿using SplashKitSDK;
+
+namespace ClosestPointOnLineFromCircle
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Window window = new Window("Closest Point On Line From Circle", 800, 600);
+
+            Point2D cursor_pos;
+            Circle circle_shape = SplashKit.CircleAt(SplashKit.PointAt(250, 150), 100);
+            Line line_shape;
+            Point2D closest_point_coordinates;
+
+            while (!SplashKit.QuitRequested())
+            {
+                SplashKit.ProcessEvents();
+                cursor_pos = SplashKit.MousePosition();
+                line_shape = SplashKit.LineFrom(SplashKit.PointAt(300, 400), cursor_pos);
+
+                // Point2D variable stores the x and y coordinates of the closest point between the circle and line
+                closest_point_coordinates = SplashKit.ClosestPointOnLineFromCircle(circle_shape, line_shape);
+
+                SplashKit.ClearScreen();
+                SplashKit.DrawCircle(SplashKit.ColorBlack(), circle_shape);
+                SplashKit.DrawLine(SplashKit.ColorBlack(), line_shape);
+                SplashKit.FillCircle(SplashKit.ColorRed(), SplashKit.CircleAt(closest_point_coordinates, 5));
+
+                SplashKit.DrawText("Position of closest point on line from circle: " + SplashKit.PointToString(closest_point_coordinates), SplashKit.ColorBlack(), 110, 500);
+                SplashKit.RefreshScreen();
+            }
+            window.Close();
+        }
+    }
+}
