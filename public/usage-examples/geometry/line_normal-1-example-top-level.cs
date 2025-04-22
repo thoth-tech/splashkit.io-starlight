bda9ff3a9d62466e971e7dc667e12968d96caa77
CATHoDE33 <94217306+CATHoDE33@users.noreply.github.com>
2025-04-23 01:45:37 +1000

    Add files via upload

---
+++ public/usage-examples/geometry/line_normal-1-example-top-level.cs
@@ -0,0 +1,32 @@
+﻿using SplashKitSDK;
+using static SplashKitSDK.SplashKit;
+
+OpenWindow("Line Normal", 800, 600);
+
+Line userLine;
+Line xAxisLine;
+Line yAxisLine;
+Point2D cursorPos;
+Vector2D vector;
+
+while (!QuitRequested())
+{
+    ProcessEvents();
+    cursorPos = MousePosition();
+    userLine = LineFrom(PointAt(400, 350), cursorPos);
+
+    xAxisLine = LineFrom(PointAt(cursorPos.X, 350), PointAt(400, 350));
+    yAxisLine = LineFrom(PointAt(400, cursorPos.Y), PointAt(400, 350));
+
+    // The line normal of the desired line is stored under the vector 2d variable
+    vector = LineNormal(userLine);
+
+    ClearScreen();
+    DrawLine(Color.Black, userLine);
+    DrawLine(Color.Red, xAxisLine);
+    DrawLine(Color.Red, yAxisLine);
+    DrawText("The black line's normal is: " + vector.X.ToString() + "," + vector.Y.ToString(), Color.Black, 60, 500);
+
+    RefreshScreen();
+}
+CloseAllWindows();
---
+++ public/usage-examples/geometry/line_normal-1-example.py
@@ -0,0 +1,31 @@
+from splashkit import *
+
+open_window("Line Normal", 800, 600)
+
+user_line = Line
+x_axis_line = Line
+y_axis_line = Line
+cursor_pos = Point2D()
+vector = Vector2D()
+
+while (not quit_requested()):
+    process_events()
+    
+    cursor_pos = mouse_position()
+    user_line = line_from_point_to_point(point_at(400, 350), cursor_pos)
+
+    x_axis_line = line_from_point_to_point(point_at(cursor_pos.x, 350), point_at(400, 350))
+    y_axis_line = line_from_point_to_point(point_at(400, cursor_pos.y), point_at(400, 350))
+
+    # The line normal of the desired line is stored under the vector 2d variable
+    vector = line_normal(user_line)
+
+    clear_screen_to_white()
+    draw_line_record(color_black(), user_line)
+    draw_line_record(color_red(), x_axis_line)
+    draw_line_record(color_red(), y_axis_line)
+    draw_text_no_font_no_size("The black line's normal is: " + str(vector.x) + "," + str(vector.y) , color_black(), 60, 500)
+
+    refresh_screen()
+    
+close_all_windows()
