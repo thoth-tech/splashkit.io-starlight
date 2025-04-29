39a6cfd0bb4aa1442d0ad757f7bbc8f5bd362a02
SimonRhook <rhooki13@gmail.com>
2025-04-29 19:38:36 +1000

    Update based on user reviews

--- public/usage-examples/graphics/draw_rectangle-1-example-beyond.cpp
+++ public/usage-examples/graphics/draw_rectangle-1-example-beyond.cpp
@@ -9,18 +9,15 @@
 
 int main(int argv, char **args)
 {
-    // Declare Variables
-    SDL_Window *window = nullptr;
-
-    // Check for successful initialisation
+    // Check if SDL was successfully initialized
     if (SDL_Init(SDL_INIT_VIDEO) < 0)
     {
         std::cout << "SDL could not be initialized: " << SDL_GetError();
         exit(1);
     }
 
-    // Create Window
-    window = SDL_CreateWindow(
+    // Create window
+    SDL_Window *window = SDL_CreateWindow(
         "Draw Rectangle",
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
@@ -28,7 +25,6 @@ int main(int argv, char **args)
         600,
         SDL_WINDOW_OPENGL);
 
-    // Error handling for window
     if (window == NULL)
     {
         SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window: %s\n", SDL_GetError());
@@ -39,7 +35,7 @@ int main(int argv, char **args)
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     if (renderer == NULL)
     {
-        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create rederer: %s\n", SDL_GetError());
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create renderer: %s\n", SDL_GetError());
         exit(1);
     }
 
@@ -49,26 +45,22 @@ int main(int argv, char **args)
 
     for (int i = 0; i < 21; i++)
     {
+        // Calculate rectangle coordinates
         int x = i * 40 - 40;
         int y = 600 - i * 30;
-        // Draw rectangle using x and y above with width 40 and height 30
+        // Select a random color and draw rectangle
         SDL_Rect rect = {x, y, 40, 30};
         SDL_SetRenderDrawColor(renderer, std::rand() % 256, std::rand() % 256, std::rand() % 256, 255);
         SDL_RenderDrawRect(renderer, &rect);
     }
-    // Display drawing
+   
+    // Display the drawing on the window and wait 5 seconds
     SDL_RenderPresent(renderer);
-
-    // Hold window 5 seconds
     SDL_Delay(5000);
 
     // Cleanup and free memory
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
-
-    // Cleanup and free memory
-    SDL_DestroyWindow(window);
-
     return 0;
 }
