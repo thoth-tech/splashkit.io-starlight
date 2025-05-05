c842c02d0dcbc0504d0080a9f41db619a4e056f3
sylvia0929 <529807748@qq.com>
2025-05-05 07:09:23 +0000

    feat: add Beyond SplashKit and rename SplashKit version of fill circle example

---
+++ public/usage-examples/graphics/fill_circle_on_window-1-example-beyond.cpp
@@ -0,0 +1,27 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(400, 400), "Fill Circle on Window");
+
+    // Define a filled circle
+    sf::CircleShape circle(50); // radius
+    circle.setFillColor(sf::Color::Red);
+    circle.setPosition(150, 150); // x, y
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear(sf::Color::White);
+        window.draw(circle);
+        window.display();
+    }
+
+    return 0;
+}
