deaebebaecc227b45564a8b656fcec92c1b55db1
pradeepkumara <prasannapradeep100@gmail.com>
2025-05-06 19:58:45 +1000

    Option line width Updated changes

--- public/usage-examples/graphics/option_line_width-1-example-sk.cpp
+++ public/usage-examples/graphics/option_line_width-1-example-sk.cpp
@@ -10,10 +10,8 @@ int main()
     
     opt = option_line_width(5);
     draw_line(COLOR_RED, 400, 100, 600, 250, opt);
-
     refresh_screen();
 
-   
     while (!quit_requested())
     {
         process_events();
--- public/usage-examples/graphics/option_line_width-1-example-top-level.cs
+++ public/usage-examples/graphics/option_line_width-1-example-top-level.cs
@@ -1,22 +1,16 @@
 using SplashKitSDK;
 
-public class Program
+//Window window = SplashKit.OpenWindow("Line Width Example", 800, 600);
+
+DrawingOptions opt = SplashKit.OptionLineWidth(10);
+SplashKit.DrawLine(Color.Black, 100, 100, 200, 200, opt);
+
+opt = SplashKit.OptionLineWidth(5);
+SplashKit.DrawLine(Color.Red, 400, 100, 600, 250, opt);
+
+SplashKit.RefreshScreen();
+
+while (!SplashKit.WindowCloseRequested(window))
 {
-    public static void Main()
-    {
-        Window window = SplashKit.OpenWindow("Line Width Example", 800, 600);
-
-        DrawingOptions opt = SplashKit.OptionLineWidth(10);
-        SplashKit.DrawLine(Color.Black, 100, 100, 200, 200, opt);
-
-        opt = SplashKit.OptionLineWidth(5);
-        SplashKit.DrawLine(Color.Red, 400, 100, 600, 250, opt);
-
-        SplashKit.RefreshScreen();
-
-        while (!SplashKit.WindowCloseRequested(window))
-        {
-            SplashKit.ProcessEvents();
-        }
-    }
+    SplashKit.ProcessEvents();
 }
