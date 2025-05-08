1a9bac1f38d4e1ac1f2f4d7a434e330e240d3427
huyhuynh2319 <huyhuynh2319@gmail.com>
2025-05-08 11:27:51 +1000

    changed

--- public/usage-examples/graphics/option_flip_x-1-simple.py
+++ public/usage-examples/graphics/option_flip_x-1-simple.py
@@ -1,9 +1,8 @@
 from splashkit import *
 
-# Open a new window with the title "Option Flip X" 
 window = open_window("Option Flip X", 600, 600)
 
-# Load a bitmap image named "Player" from the file "character.png"
+
 bmp = load_bitmap("Player", "character.png")
 
 # Draw the original bitmap image at position (100, 300) in the window
@@ -12,11 +11,8 @@ draw_bitmap(bmp, 100, 300)
 # Draw the bitmap image flipped horizontally at position (500, 300) 
 draw_bitmap_on_window_with_options(window, bmp, 500, 300, option_flip_xy())
 
-# Refresh the screen to display the drawings
 refresh_screen()
 
-# Wait for 5 seconds before closing the window
 delay(5000)
 
-# Close all open windows
 close_all_windows()
