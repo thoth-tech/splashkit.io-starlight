eebfd78951939b9db6a35fdc6ce62435cc2d6ca5
Trung Pham <tuantrung6d04@gmail.com>
2025-05-15 22:26:32 +1000

    completed some changes

---
+++ public/usage-examples/interface/button-1-example.py
@@ -0,0 +1,25 @@
+from splashkit import *
+
+open_window("Background Color Toggle Button", 600, 400)
+
+# Define the background color and button rectangle
+bg_color = color_white()
+btn_rect = rectangle_from(200, 180, 200, 40)
+
+# Main loop
+while (not quit_requested()):
+    process_events()
+
+    # If the button is clicked, toggle the background color
+    if button_at_position("Click Me!", btn_rect):
+        if bg_color == color_white():
+            bg_color = color_light_blue()
+        else:
+            bg_color = color_white()
+    # Clear screen and draw interface
+    clear_screen(bg_color)
+    button_at_position("Click Me!", btn_rect)
+    draw_interface()
+    refresh_screen()
+
+close_all_windows()
