335a3bd7c8985aec90b91b7e7ee541d0f5a20212
JulianBurton <burtonju@deakin.edu.au>
2025-05-15 14:19:53 +1000

    Overload for Get_Font_style

---
+++ public/usage-examples/graphics/get_font_style-1-example-top-level.cs
@@ -0,0 +1,63 @@
+﻿using SplashKitSDK;
+using static SplashKitSDK.SplashKit;
+
+OpenWindow("Interactive Font Style Changer", 800, 120);
+Font arial = LoadFont("Arial", "Arial.TTF");
+
+// Initialise Default message
+string infoText = "Press N for Normal, B for Bold, I for Italics, or U for Underlined.";
+string fontText = "";
+FontStyle style = FontStyle.NormalFont;
+
+while (!SplashKit.QuitRequested())
+{
+    ProcessEvents();
+
+    // Check key presses and update font style
+    if (KeyTyped(KeyCode.NKey))
+    {
+        SetFontStyle(arial, FontStyle.NormalFont);
+    }
+    else if (KeyTyped(KeyCode.BKey))
+    {
+        SetFontStyle(arial, FontStyle.BoldFont);
+    }
+    else if (KeyTyped(KeyCode.IKey))
+    {
+        SetFontStyle(arial, FontStyle.ItalicFont);
+    }
+    else if (KeyTyped(KeyCode.UKey))
+    {
+        SetFontStyle(arial, FontStyle.UnderlineFont);
+    }
+
+    fontText = $"Font style set to ";
+    style = GetFontStyle(arial);
+    switch (style)
+    {
+        case FontStyle.NormalFont:
+            fontText += "Normal";
+            break;
+        case FontStyle.BoldFont:
+            fontText += "Bold";
+            break;
+        case FontStyle.ItalicFont:
+            fontText += "Italic";
+            break;
+        case FontStyle.UnderlineFont:
+            fontText += "Underlined";
+            break;
+        default:
+            fontText += "Unknown";
+            break;
+    }
+
+    // Clear screen and draw updated message
+    ClearScreen(Color.White);
+    DrawText(infoText, Color.Black, arial, 20, 50, 20);
+    DrawText(fontText, Color.Black, arial, 20, 50, 80);
+    RefreshScreen();
+}
+
+Delay(5000);
+CloseAllWindows();
