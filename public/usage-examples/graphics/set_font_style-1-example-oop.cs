d572b965c75741ceef6df00029127edae0031b70
JulianBurton <burtonju@deakin.edu.au>
2025-05-17 14:39:19 +1000

    Finished overload for set font style modeled off the other overload

---
+++ public/usage-examples/graphics/set_font_style-1-example-oop.cs
@@ -0,0 +1,45 @@
+﻿using SplashKitSDK;
+
+public class Program
+{
+    public static void Main()
+    {
+        SplashKit.OpenWindow("Font Style Changer", 800, 120);
+        // Load Font
+        Font Arial = SplashKit.LoadFont("Arial", "Arial.TTF");
+
+        // Default Message
+        string instructions = "Press N for Normal, B for Bold, I for Italics, or U for Underlined.";
+
+        while (!SplashKit.QuitRequested())
+        {
+            SplashKit.ProcessEvents();
+
+            // Check key presses and update font style and message
+            if (SplashKit.KeyTyped(KeyCode.NKey))
+            {
+                SplashKit.SetFontStyle(Arial, FontStyle.NormalFont);
+            }
+            else if (SplashKit.KeyTyped(KeyCode.BKey))
+            {
+                SplashKit.SetFontStyle(Arial, FontStyle.BoldFont);
+            }
+            else if (SplashKit.KeyTyped(KeyCode.IKey))
+            {
+                SplashKit.SetFontStyle(Arial, FontStyle.ItalicFont);
+            }
+            else if (SplashKit.KeyTyped(KeyCode.UKey))
+            {
+                SplashKit.SetFontStyle(Arial, FontStyle.UnderlineFont);
+            }
+
+            // Clear screen and draw updated message
+            SplashKit.ClearScreen(Color.White);
+            SplashKit.DrawText(instructions, Color.Black, Arial, 20, 50, 20);
+            SplashKit.RefreshScreen();
+        }
+
+        SplashKit.Delay(5000);
+        SplashKit.CloseAllWindows();
+    }
+}
---
+++ public/usage-examples/graphics/set_font_style-1-example-top-level.cs
@@ -0,0 +1,39 @@
+﻿using SplashKitSDK;
+using static SplashKitSDK.SplashKit;
+
+OpenWindow("Font Style Changer", 800, 120);
+Font arial = LoadFont("Arial", "Arial.TTF");
+
+// Initialise Default message
+string instructions = "Press N for Normal, B for Bold, I for Italics, or U for Underlined.";
+
+while (!QuitRequested())
+{
+    ProcessEvents();
+
+    // Check key presses and update font style
+    if (KeyTyped(KeyCode.NKey))
+    {
+        SetFontStyle(arial, FontStyle.NormalFont);
+    }
+    else if (KeyTyped(KeyCode.BKey))
+    {
+        SetFontStyle(arial, FontStyle.BoldFont);
+    }
+    else if (KeyTyped(KeyCode.IKey))
+    {
+        SetFontStyle(arial, FontStyle.ItalicFont);
+    }
+    else if (KeyTyped(KeyCode.UKey))
+    {
+        SetFontStyle(arial, FontStyle.UnderlineFont);
+    }
+
+    // Clear screen and draw updated message
+    ClearScreen(Color.White);
+    DrawText(instructions, Color.Black, arial, 20, 50, 20);
+    RefreshScreen();
+}
+
+Delay(5000);
+CloseAllWindows();
