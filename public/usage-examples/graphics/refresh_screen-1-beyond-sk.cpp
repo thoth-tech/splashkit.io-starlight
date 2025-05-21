ea201364ede0fbf657736c5d07c74be5ee978a2e
sakshamror <133167596+sakshamror@users.noreply.github.com>
2025-05-21 13:05:13 +1000

    Update refresh_screen-1-example-beyond.cpp

--- public/usage-examples/graphics/refresh_screen-1-beyond-sk.cpp
+++ public/usage-examples/graphics/refresh_screen-1-beyond-sk.cpp
@@ -1,71 +1,81 @@
+// refresh_screen-1-example-beyond.cpp
+
 #define SDL_MAIN_HANDLED
 #include <SDL2/SDL.h>
+#include <SDL2/SDL2_gfxPrimitives.h>
 #include <chrono>
 #include <thread>
 
-// Simple delay function
-void delay_ms(int milliseconds) {
-    std::this_thread::sleep_for(std::chrono::milliseconds(milliseconds));
+void delay_ms(int ms) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(ms));
 }
 
-// Function to draw a filled triangle using lines
 void draw_triangle(SDL_Renderer* renderer) {
-    // Define the triangle points (roof)
-    SDL_Point top = {400, 150};     // Top point
-    SDL_Point left = {250, 300};    // Left bottom
-    SDL_Point right = {550, 300};   // Right bottom
-
-    // Draw lines to fill the triangle row by row
-    for (int y = top.y; y <= left.y; y++) {
-        float ratio = (float)(y - top.y) / (left.y - top.y);
+    SDL_Point top   = {400,150};
+    SDL_Point left  = {250,300};
+    SDL_Point right = {550,300};
+    for (int y = top.y; y <= left.y; ++y) {
+        float ratio = float(y - top.y) / float(left.y - top.y);
         int startX = top.x + (left.x - top.x) * ratio;
-        int endX = top.x + (right.x - top.x) * ratio;
+        int endX   = top.x + (right.x - top.x) * ratio;
         SDL_RenderDrawLine(renderer, startX, y, endX, y);
     }
 }
 
 int main() {
-    SDL_Init(SDL_INIT_VIDEO); // Start SDL
+    auto wait = [&](int ms) {
+        Uint32 start = SDL_GetTicks();
+        SDL_Event evt;
+        while (SDL_GetTicks() - start < ms) {
+            while (SDL_PollEvent(&evt)) {
+                if (evt.type == SDL_QUIT) return false;
+            }
+            SDL_Delay(16);
+        }
+        return true;
+    };
 
-    // Create a window
-    SDL_Window* window = SDL_CreateWindow("Simple House Drawing",
-                                          SDL_WINDOWPOS_CENTERED,
-                                          SDL_WINDOWPOS_CENTERED,
-                                          800, 600, 0);
-
-    // Create a renderer to draw on the window
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) return 1;
+    SDL_Window*   window   = SDL_CreateWindow(
+        "Simple House Drawing",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        800, 600, 0
+    );
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-    // Clear the screen with white
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // White
+    SDL_Rect ground, house;
+
+    if (!window || !renderer) goto cleanup;
+
+    // clear to white
+    SDL_SetRenderDrawColor(renderer, 255,255,255,255);
     SDL_RenderClear(renderer);
     SDL_RenderPresent(renderer);
-    delay_ms(1000);
+    if (!wait(1000)) goto cleanup;
 
-    // Draw green ground (rectangle)
-    SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255); // Green
-    SDL_Rect ground = {0, 400, 800, 200};
+    // draw ground
+    ground = {  0, 400, 800, 200 };
+    SDL_SetRenderDrawColor(renderer,   0,255,   0,255);
     SDL_RenderFillRect(renderer, &ground);
     SDL_RenderPresent(renderer);
-    delay_ms(1000);
+    if (!wait(1000)) goto cleanup;
 
-    // Draw gray house (rectangle)
-    SDL_SetRenderDrawColor(renderer, 150, 150, 150, 255); // Gray
-    SDL_Rect house = {300, 300, 200, 200};
+    // draw house body
+    house  = {300, 300, 200, 200};
+    SDL_SetRenderDrawColor(renderer, 150,150,150,255);
     SDL_RenderFillRect(renderer, &house);
     SDL_RenderPresent(renderer);
-    delay_ms(1000);
+    if (!wait(1000)) goto cleanup;
 
-    // Draw red roof (triangle)
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255); // Red
+    // draw roof
+    SDL_SetRenderDrawColor(renderer, 255,   0,   0,255);
     draw_triangle(renderer);
     SDL_RenderPresent(renderer);
-    delay_ms(5000);
+    if (!wait(5000)) goto cleanup;
 
-    // Clean up
+cleanup:
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
-
     return 0;
 }
