91095660939cc8a24b78707de67209cc28ea621d
Tuan Trung Pham <tuantrung6d04@gmail.com>
2025-05-25 14:36:06 +1000

    changes

--- public/usage-examples/graphics/fill_rectangle_on_window-1-example-beyond.cpp
+++ public/usage-examples/graphics/fill_rectangle_on_window-1-example-beyond.cpp
@@ -1,72 +1,81 @@
 #include <iostream>
+#define SDL_MAIN_HANDLED
 #ifdef __APPLE__
-#  include <SDL.h>
+#include <SDL.h>
 #else
-#  include <SDL2/SDL.h>
+#include <SDL2/SDL.h>
 #endif
-
-SDL_Window* sdl_open_window(const char* title, int w, int h)
-{
-    if (SDL_Init(SDL_INIT_VIDEO) < 0)
-    {
-        std::cerr << "SDL_Init failed: " << SDL_GetError() << "\n";
-        exit(1);
-    }
-    SDL_Window* win = SDL_CreateWindow(
-        title,
-        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-        w, h, SDL_WINDOW_SHOWN
-    );
-    if (!win)
-    {
-        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "SDL_CreateWindow: %s", SDL_GetError());
-        SDL_Quit();
-        exit(1);
-    }
-    return win;
-}
 
 int main()
 {
-    SDL_Window* blue_window = sdl_open_window("Blue Rectangle", 200, 200);
-    SDL_Renderer* blue_renderer = SDL_CreateReblue_window, -1, SDL_RENDERER_ACCELERATED);
-    SDL_Window* red_window  = sdl_open_window("Red Rectangle",  200, 200);
-    SDL_Renderer* red_renderer  = SDL_CreateRenderer(red_window,  -1, SDL_RENDERER_ACCELERATED);
+    // Declare windows and renderers
+    SDL_Window *blue_window = nullptr, *red_window = nullptr;
+    SDL_Renderer *blue_renderer = nullptr, *red_renderer = nullptr;
 
+    // Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        std::cerr << "SDL could not be initialized: " << SDL_GetError() << "\n";
+        return 1;
+    }
+
+    // Create Blue Window and Renderer
+    blue_window = SDL_CreateWindow("Blue Rectangle", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 200, 200, SDL_WINDOW_SHOWN);
+    if (!blue_window)
+    {
+        std::cerr << "Could not create Blue window: " << SDL_GetError() << "\n";
+        SDL_Quit();
+        return 1;
+    }
+    blue_renderer = SDL_CreateRenderer(blue_window, -1, SDL_RENDERER_ACCELERATED);
+
+    // Create Red Window and Renderer
+    red_window = SDL_CreateWindow("Red Rectangle", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 200, 200, SDL_WINDOW_SHOWN);
+    if (!red_window)
+    {
+        std::cerr << "Could not create Red window: " << SDL_GetError() << "\n";
+        SDL_DestroyRenderer(blue_renderer);
+        SDL_DestroyWindow(blue_window);
+        SDL_Quit();
+        return 1;
+    }
+    red_renderer = SDL_CreateRenderer(red_window, -1, SDL_RENDERER_ACCELERATED);
+
+    // Move Red Window to the right of Blue Window
     int x, y;
     SDL_GetWindowPosition(blue_window, &x, &y);
-    SDL_SetWindowPosition(red_window, x + 200, y);
+    SDL_SetWindowPosition(red_window, x + 210, y); // 210 = 200 width + 10px gap
 
-    // Fill a blue rectangle on the first window
+    // Blue window content
     SDL_SetRenderDrawColor(blue_renderer, 255, 255, 255, 255);
     SDL_RenderClear(blue_renderer);
-    SDL_SetRenderDrawColor(blue_renderer,   0,   0, 255, 255);
-
-    // Rendering a blue rectangle
-    SDL_Rect rect = { 50, 50, 100, 50 };
+    SDL_SetRenderDrawColor(blue_renderer, 0, 0, 255, 255);
+    SDL_Rect rect = {50, 50, 100, 50};
     SDL_RenderFillRect(blue_renderer, &rect);
     SDL_RenderPresent(blue_renderer);
 
-    // Fill a red rectangle on the second window
+    // Red window content
     SDL_SetRenderDrawColor(red_renderer, 255, 255, 255, 255);
     SDL_RenderClear(red_renderer);
-    SDL_SetRenderDrawColor(red_renderer, 255,   0,   0, 255);
+    SDL_SetRenderDrawColor(red_renderer, 255, 0, 0, 255);
     SDL_RenderFillRect(red_renderer, &rect);
     SDL_RenderPresent(red_renderer);
 
-    // Keep windows open and responsive for 5 seconds
-    Uint32 start = SDL_GetTicks();
-    SDL_Event ev;
-    while (SDL_GetTicks() - start < 5000)
+    // Keep windows open for 4 seconds
+    Uint32 start_time = SDL_GetTicks();
+    SDL_Event event;
+    while (SDL_GetTicks() - start_time < 4000)
     {
-        while (SDL_PollEvent(&ev)) {}
-        SDL_Delay(16);
+        if (SDL_PollEvent(&event) && event.type == SDL_QUIT)
+            break;
     }
 
+    // Cleanup
     SDL_DestroyRenderer(blue_renderer);
-    SDL_Destroy(blue_window);
     SDL_DestroyRenderer(red_renderer);
+    SDL_DestroyWindow(blue_window);
     SDL_DestroyWindow(red_window);
     SDL_Quit();
+
     return 0;
 }
