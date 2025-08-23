3dbad6848a0dac558dceb06e67c76afd8fda52ef
CATHoDE33 <94217306+CATHoDE33@users.noreply.github.com>
2025-08-24 00:36:29 +1000

    Add files via upload

---
+++ public/usage-examples/graphics/bitmap_height-1-example-top-level.cs
@@ -0,0 +1,15 @@
+﻿using SplashKitSDK;
+using static SplashKitSDK.SplashKit;
+
+OpenWindow("Bitmap Height", 800, 600);
+
+Bitmap imageBitmap = LoadBitmap("imageBitmap", "image1.jpg");
+
+ClearScreen(ColorWhite());
+DrawBitmap(imageBitmap, 200, 155);
+DrawText($"The above bitmap is {BitmapHeight(imageBitmap)} pixels in height", ColorBlack(), 215, 450);
+RefreshScreen();
+
+Delay(5000);
+
+CloseAllWindows();
