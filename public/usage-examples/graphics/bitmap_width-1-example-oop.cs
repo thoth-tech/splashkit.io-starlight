a7c95c3c67e9f04f0965b18107bd84f384fd9839
CATHoDE33 <94217306+CATHoDE33@users.noreply.github.com>
2025-08-25 18:36:54 +1000

    Add files via upload

---
+++ public/usage-examples/graphics/bitmap_width-1-example-oop.cs
@@ -0,0 +1,23 @@
+﻿using SplashKitSDK;
+
+namespace BitmapWidthExample
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            SplashKit.OpenWindow("Bitmap Width", 800, 600);
+
+            Bitmap imageBitmap = SplashKit.LoadBitmap("imageBitmap", "image1.jpg");
+
+            SplashKit.ClearScreen(Color.White);
+            SplashKit.DrawBitmap(imageBitmap, 200, 155);
+            SplashKit.DrawText($"The above bitmap is {SplashKit.BitmapWidth(imageBitmap)} pixels in width", Color.Black, 215, 450);
+            SplashKit.RefreshScreen();
+
+            SplashKit.Delay(5000);
+
+            SplashKit.CloseAllWindows();
+        }
+    }
+}
---
+++ public/usage-examples/graphics/bitmap_width-1-example-top-level.cs
@@ -0,0 +1,15 @@
+﻿using SplashKitSDK;
+using static SplashKitSDK.SplashKit;
+
+OpenWindow("Bitmap Width", 800, 600);
+
+Bitmap imageBitmap = LoadBitmap("imageBitmap", "image1.jpg");
+
+ClearScreen(ColorWhite());
+DrawBitmap(imageBitmap, 200, 155);
+DrawText($"The above bitmap is {BitmapWidth(imageBitmap)} pixels in width", ColorBlack(), 215, 450);
+RefreshScreen();
+
+Delay(5000);
+
+CloseAllWindows();
