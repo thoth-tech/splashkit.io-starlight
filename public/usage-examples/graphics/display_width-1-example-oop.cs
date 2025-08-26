88320d4c3b9dc39668bef01fac6cabaf5a2cdec4
CATHoDE33 <94217306+CATHoDE33@users.noreply.github.com>
2025-08-26 23:05:18 +1000

    Add files via upload

---
+++ public/usage-examples/graphics/display_width-1-example-oop.cs
@@ -0,0 +1,29 @@
+﻿using SplashKitSDK;
+
+namespace DisplayWidthExample
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Display display = SplashKit.DisplayDetails(0);
+            int width = SplashKit.DisplayWidth(display);
+
+            SplashKit.OpenWindow("Display Width", width, 600);
+
+            Font font = SplashKit.FontNamed("Century.ttf");
+            string text = $"The display is {width} pixels wide";
+            
+            SplashKit.ClearScreen(Color.White);
+            SplashKit.FillRectangle(Color.Black, 0, SplashKit.ScreenHeight() / 2, width, 1);
+            SplashKit.DrawText("<", Color.Black, -2, SplashKit.ScreenHeight() / 2 - 3);
+            SplashKit.DrawText(">", Color.Black, width - 6, SplashKit.ScreenHeight() / 2 - 3);
+            SplashKit.DrawText(text, Color.Black, font, 30, (SplashKit.ScreenWidth() / 2) - (SplashKit.TextWidth(text, font, 30) / 2), SplashKit.ScreenHeight() / 2 - 60);
+            SplashKit.RefreshScreen();
+
+            SplashKit.Delay(5000);
+
+            SplashKit.CloseAllWindows();
+        }
+    }
+}
---
+++ public/usage-examples/graphics/display_width-1-example-top-level.cs
@@ -0,0 +1,21 @@
+﻿using SplashKitSDK;
+using static SplashKitSDK.SplashKit;
+
+Display display = SplashKit.DisplayDetails(0);
+int width = SplashKit.DisplayWidth(display);
+
+OpenWindow("Display Width", width, 600);
+
+Font font = FontNamed("Century.ttf");
+string text = $"The display is {width} pixels wide";
+
+ClearScreen(ColorWhite());
+FillRectangle(ColorBlack(), 0, ScreenHeight() / 2, width, 1);
+DrawText("<", ColorBlack(), -2, ScreenHeight() / 2 - 3);
+DrawText(">", ColorBlack(), width - 6, ScreenHeight() / 2 - 3);
+DrawText(text, ColorBlack(), font, 30, (ScreenWidth() / 2) - (TextWidth(text, font, 30) / 2), ScreenHeight() / 2 - 60);
+RefreshScreen();
+
+Delay(5000);
+
+CloseAllWindows();
