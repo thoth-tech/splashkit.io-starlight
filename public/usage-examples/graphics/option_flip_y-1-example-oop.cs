d8326d8da8829e88e779a1c56fa7d10dc2642332
CATHoDE33 <94217306+CATHoDE33@users.noreply.github.com>
2025-08-27 22:56:34 +1000

    Add files via upload

---
+++ public/usage-examples/graphics/option_flip_y-1-example-oop.cs
@@ -0,0 +1,24 @@
+﻿using SplashKitSDK;
+
+namespace OptionFlipYExample
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            SplashKit.OpenWindow("Option Flip Y", 800, 600);
+
+            Bitmap imageBitmap = SplashKit.LoadBitmap("image_bitmap", "image1.jpg");
+
+            SplashKit.ClearScreen(Color.White);
+            // Function used here ↓
+            SplashKit.DrawBitmap(imageBitmap, 200, 155, SplashKit.OptionFlipY());
+            SplashKit.DrawText("This bitmap has been flipped along it's Y axis", Color.Black, 215, 450);
+            SplashKit.RefreshScreen();
+
+            SplashKit.Delay(5000);
+
+            SplashKit.CloseAllWindows();
+        }
+    }
+}
---
+++ public/usage-examples/graphics/option_flip_y-1-example-top-level.cs
@@ -0,0 +1,16 @@
+﻿using SplashKitSDK;
+using static SplashKitSDK.SplashKit;
+
+OpenWindow("Option Flip Y", 800, 600);
+
+Bitmap imageBitmap = LoadBitmap("image_bitmap", "image1.jpg");
+
+ClearScreen(ColorWhite());
+// Function used here ↓
+DrawBitmap(imageBitmap, 200, 155, OptionFlipY());
+DrawText("This bitmap has been flipped along it's Y axis", ColorBlack(), 215, 450);
+RefreshScreen();
+
+Delay(5000);
+
+CloseAllWindows();
