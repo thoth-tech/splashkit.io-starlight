3d32817cdbbdc2b01fc753ee7cf9c0ee83eaf1ea
CATHoDE33 <94217306+CATHoDE33@users.noreply.github.com>
2025-08-28 23:57:26 +1000

    Add files via upload

---
+++ public/usage-examples/graphics/save_bitmap-1-example-oop.cs
@@ -0,0 +1,38 @@
+﻿using SplashKitSDK;
+
+namespace SaveBitmapExample
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            SplashKit.OpenWindow("Save Bitmap", 800, 600);
+
+            int opacityValue = 0;
+            Bitmap imageBitmap = SplashKit.LoadBitmap("imageBitmap", "image1.jpg");
+
+            while (!SplashKit.QuitRequested())
+            {
+                SplashKit.ProcessEvents();
+                if (SplashKit.KeyTyped(KeyCode.ReturnKey))
+                {
+                    SplashKit.SaveBitmap(imageBitmap, "savedBitmap");
+                    opacityValue = 2500;
+                }
+
+                if (opacityValue != 0)
+                {
+                    opacityValue -= 1;
+                }
+
+                SplashKit.ClearScreen();
+                SplashKit.DrawBitmap(imageBitmap, 200, 155);
+                SplashKit.DrawText("Press the 'Enter' key to save the above bitmap to desktop", Color.Black, 175, 450);
+                SplashKit.DrawText("Image saved to desktop!", SplashKit.RGBAColor(0, 0, 0, opacityValue), 310, 470);
+
+                SplashKit.RefreshScreen();
+            }
+            SplashKit.CloseAllWindows();
+        }
+    }
+}
