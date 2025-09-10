47d499bde4b2a0430bcba73333a26b12b7b0e8c5
CATHoDE33 <94217306+CATHoDE33@users.noreply.github.com>
2025-09-10 18:45:52 +1000

    Add files via upload

--- public/usage-examples/color/string_to_color-1-example-oop.cs
+++ public/usage-examples/color/string_to_color-1-example-oop.cs
@@ -9,18 +9,14 @@ namespace StringToColorExample
             SplashKit.OpenWindow("String To Color", 800, 600);
 
             // Change this string to get different colors 
-            string color_hex = "#921e64d9";
-            // Function used here ↓
-            Color color = SplashKit.StringToColor(color_hex);
-            int red_component = SplashKit.RedOf(color);
-            int green_component = SplashKit.GreenOf(color);
-            int blue_component = SplashKit.BlueOf(color);
+            string colorHex = "#921e64d9";
+            Color color = SplashKit.StringToColor(colorHex);
             Rectangle rectangle = SplashKit.RectangleFrom(200, 100, 400, 300);
 
             SplashKit.ClearScreen(Color.White);
             SplashKit.FillRectangle(color, rectangle);
-            SplashKit.DrawText("Current color's RGBA hex value is " + color_hex, Color.Black, 235, 450);
-            SplashKit.DrawText("It's RGB values are: R-" + red_component.ToString() + ", G-" + green_component.ToString() + ", B-" + blue_component.ToString(), Color.Black, 235, 470);
+            SplashKit.DrawText("Current color's RGBA hex value is " + colorHex, Color.Black, 235, 450);
+            SplashKit.DrawText("It's RGB values are: R-" + SplashKit.RedOf(color) + ", G-" + SplashKit.GreenOf(color) + ", B-" + SplashKit.BlueOf(color), Color.Black, 235, 470);
             SplashKit.RefreshScreen();
 
             SplashKit.Delay(5000);
--- public/usage-examples/color/string_to_color-1-example.cpp
+++ public/usage-examples/color/string_to_color-1-example.cpp
@@ -3,19 +3,15 @@
 int main()
 {
     open_window("String To Color", 800, 600);
-    // Change this string to get different colors 
+    // Change this string's value to produce different colors ↓
     string color_hex = "#921e64d9";
-    // Function used here ↓
     color color = string_to_color(color_hex);
-    int red_component = red_of(color);
-    int green_component = green_of(color);
-    int blue_component = blue_of(color);
     rectangle rectangle = rectangle_from(200, 100, 400, 300);
 
     clear_screen(color_white());
     fill_rectangle(color, rectangle);
     draw_text("Current color's RGBA hex value is " + color_hex, color_black(), 235, 450);
-    draw_text("It's RGB values are: R-" + std::to_string(red_component) + ", G-" + std::to_string(green_component) + ", B-" + std::to_string(blue_component), color_black(), 235, 470);
+    draw_text("It's RGB values are: R-" + std::to_string(red_of(color)) + ", G-" + std::to_string(green_of(color)) + ", B-" + std::to_string(blue_of(color)), color_black(), 235, 470);
     refresh_screen();
 
     delay(5000);
