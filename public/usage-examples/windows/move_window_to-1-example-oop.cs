a3c671835b89bc5741586e8c1884c6ffe0868c35
JulianBurton <burtonju@deakin.edu.au>
2025-09-11 19:27:34 +1000

    finally

---
+++ public/usage-examples/windows/move_window_to-1-example-oop.cs
@@ -0,0 +1,71 @@
+﻿using SplashKitSDK;
+
+namespace MoveWindowToExample
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Window win = SplashKit.OpenWindow("Window Mover", 300, 300);
+
+            while (!SplashKit.QuitRequested())
+            {
+                // get user inputs
+                SplashKit.ProcessEvents();
+
+                SplashKit.ClearScreen(Color.White);
+
+                // Get position of the window
+                int currentX = SplashKit.WindowX(win);
+                int currentY = SplashKit.WindowY(win);
+
+                // Move window buttons
+                if (SplashKit.Button("NW", SplashKit.RectangleFrom(50, 50, 40, 40)))
+                {
+                    SplashKit.MoveWindowTo(win, currentX - 10, currentY - 10);
+                }
+
+                if (SplashKit.Button("N", SplashKit.RectangleFrom(130, 50, 40, 40)))
+                {
+                    SplashKit.MoveWindowTo(win, currentX, currentY - 10);
+                }
+
+                if (SplashKit.Button("NE", SplashKit.RectangleFrom(210, 50, 40, 40)))
+                {
+                    SplashKit.MoveWindowTo(win, currentX + 10, currentY - 10);
+                }
+
+                if (SplashKit.Button("W", SplashKit.RectangleFrom(50, 130, 40, 40)))
+                {
+                    SplashKit.MoveWindowTo(win, currentX - 10, currentY);
+                }
+
+                if (SplashKit.Button("E", SplashKit.RectangleFrom(210, 130, 40, 40)))
+                {
+                    SplashKit.MoveWindowTo(win, currentX + 10, currentY);
+                }
+
+                if (SplashKit.Button("SW", SplashKit.RectangleFrom(50, 210, 40, 40)))
+                {
+                    SplashKit.MoveWindowTo(win, currentX - 10, currentY + 10);
+                }
+
+                if (SplashKit.Button("S", SplashKit.RectangleFrom(130, 210, 40, 40)))
+                {
+                    SplashKit.MoveWindowTo(win, currentX, currentY + 10);
+                }
+
+                if (SplashKit.Button("SE", SplashKit.RectangleFrom(210, 210, 40, 40)))
+                {
+                    SplashKit.MoveWindowTo(win, currentX + 10, currentY + 10);
+                }
+
+                SplashKit.DrawInterface();
+                SplashKit.RefreshScreen();
+            }
+
+            // close all open windows
+            SplashKit.CloseAllWindows();
+        }
+    }
+}
---
+++ public/usage-examples/windows/move_window_to-1-example-top-level.cs
@@ -0,0 +1,62 @@
+﻿using SplashKitSDK
+using static SplashKitSDK.SplashKit;
+
+Window win = OpenWindow("Window Mover", 300, 300);
+while (!QuitRequested())
+{
+    // Get user inputs
+    ProcessEvents();
+
+    ClearScreen(ColorWhite());
+
+    // Get position of the window
+    int currentX = WindowX(win);
+    int currentY = WindowY(win);
+
+    // Move window buttons
+    if (Button("NW", RectangleFrom(50, 50, 40, 40)))
+    {
+        MoveWindowTo(win, currentX - 10, currentY - 10);
+    }
+
+    if (Button("N", RectangleFrom(130, 50, 40, 40)))
+    {
+        MoveWindowTo(win, currentX, currentY - 10);
+    }
+
+    if (Button("NE", RectangleFrom(210, 50, 40, 40)))
+    {
+        MoveWindowTo(win, currentX + 10, currentY - 10);
+    }
+
+    if (Button("W", RectangleFrom(50, 130, 40, 40)))
+    {
+        MoveWindowTo(win, currentX - 10, currentY);
+    }
+
+    if (Button("E", RectangleFrom(210, 130, 40, 40)))
+    {
+        MoveWindowTo(win, currentX + 10, currentY);
+    }
+
+    if (Button("SW", RectangleFrom(50, 210, 40, 40)))
+    {
+        MoveWindowTo(win, currentX - 10, currentY + 10);
+    }
+
+    if (Button("S", RectangleFrom(130, 210, 40, 40)))
+    {
+        MoveWindowTo(win, currentX, currentY + 10);
+    }
+
+    if (Button("SE", RectangleFrom(210, 210, 40, 40)))
+    {
+        MoveWindowTo(win, currentX + 10, currentY + 10);
+    }
+
+    DrawInterface();
+    RefreshScreen();
+}
+
+// close all open windows
+CloseAllWindows();
