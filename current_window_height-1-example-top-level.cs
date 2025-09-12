cd9a1b9066705ea722a5ed0fe5acbdc475d2f3fa
JulianBurton <167053735+JulianBurton@users.noreply.github.com>
2025-09-12 12:13:07 +1000

    Add files via upload

---
+++ current_window_height-1-example-top-level.cs
@@ -0,0 +1,14 @@
+﻿using SplashKitSDK;
+using static SplashKitSDK.SplashKit;
+
+Random rnd = new Random();
+
+OpenWindow("Random Window Height", 275, rnd.Next(100, 800));
+
+ClearScreen(ColorWhite());
+DrawText($"This window is {CurrentWindowHeight()} pixels tall", ColorBlack(), 20, 20);
+RefreshScreen();
+
+Delay(5000);
+
+CloseAllWindows();
