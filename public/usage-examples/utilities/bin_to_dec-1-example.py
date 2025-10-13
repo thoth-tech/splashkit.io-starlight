07edeea1fde9b213eef47ae987b59f7c2dcfb5bd
Muhammad Danial <mdanial@deakin.edu.au>
2025-10-13 03:11:58 +0200

    Add BinToDec usage example for SplashKit utilities

---
+++ public/usage-examples/utilities/bin_to_dec-1-example.py
@@ -0,0 +1,7 @@
+from splashkit import write_line, bin_to_dec
+
+binary_value = "1111010"
+decimal_value = bin_to_dec(binary_value)  # SplashKit function
+
+write_line("Binary: " + binary_value)
+write_line("Decimal: " + str(decimal_value))
