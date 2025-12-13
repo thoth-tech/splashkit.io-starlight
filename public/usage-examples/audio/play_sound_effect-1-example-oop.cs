d89a4fca1a888fedd9b1a3513ec63e51bd03b950
222448082Ashen <167728161+222448082Ashen@users.noreply.github.com>
2025-12-13 22:00:24 +0530

    Revert "Remove play sound effect audio usage examples"
    This reverts commit 2259ed6804e5e25692d586bb84c7ef0025463e57.

---
+++ public/usage-examples/audio/play_sound_effect-1-example-oop.cs
@@ -0,0 +1,17 @@
+using System;
+using SplashKitSDK;
+
+public class PlaySoundExample
+{
+    public static void Main()
+    {
+        SplashKit.OpenAudio();
+
+        SplashKit.LoadSoundEffect("beep", "beep.ogg");
+        SoundEffect s = SplashKit.SoundEffectNamed("beep");
+        SplashKit.PlaySoundEffect(s);
+
+        System.Threading.Thread.Sleep(1200);
+        SplashKit.CloseAudio();
+    }
+}
