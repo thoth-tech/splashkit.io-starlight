ec9289dadf9d1152083b7f089a8c73bc80eacd66
Sparshdeep Singh <66513733+Sparsh300@users.noreply.github.com>
2025-12-13 20:52:38 +1100

    feat(usage-examples): add usage example code and description files

---
+++ public/usage-examples/sprites/create_sprite-01-example-oop.cs
@@ -0,0 +1,15 @@
+using SplashKitSDK;
+
+public static class Program {
+  public static void Main() {
+    new Window("Sprite Example", 800, 600);
+    Bitmap bmp = SplashKit.LoadBitmap("player", "images/player.png");
+    Sprite player = SplashKit.CreateSprite(bmp);
+    while (!SplashKit.QuitRequested()) {
+      SplashKit.ProcessEvents();
+      SplashKit.ClearScreen(Color.White);
+      SplashKit.DrawSprite(player);
+      SplashKit.RefreshScreen();
+    }
+  }
+}
