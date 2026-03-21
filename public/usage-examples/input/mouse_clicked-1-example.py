4adf550d5b8fd704bdbefaec3f025b6738b4f78e
Osaid2993 <osaidbahadry@gmail.com>
2026-03-21 16:03:41 +0000

    Add mouse_clicked usage example

---
+++ public/usage-examples/input/mouse_clicked-1-example.py
@@ -0,0 +1,45 @@
+from splashkit import *
+
+open_window("Circle Click Display", 800, 600)
+
+clicked_circle = "None"
+click_count = 0
+
+while not quit_requested():
+    process_events()
+
+    if mouse_clicked(LEFT_BUTTON):
+        mouse_point = mouse_position()
+
+        if point_in_circle(mouse_point, circle_at(180, 250, 60)):
+            clicked_circle = "Red"
+            click_count += 1
+        elif point_in_circle(mouse_point, circle_at(400, 250, 60)):
+            clicked_circle = "Blue"
+            click_count += 1
+        elif point_in_circle(mouse_point, circle_at(620, 250, 60)):
+            clicked_circle = "Green"
+            click_count += 1
+
+    clear_screen(COLOR_WHITE)
+
+    draw_text("Click a circle to see which one was selected.", COLOR_BLACK, 20, 20)
+    draw_text("Last clicked: " + clicked_circle, COLOR_BLACK, 20, 60)
+    draw_text("Total clicks: " + str(click_count), COLOR_BLACK, 20, 100)
+
+    fill_circle(COLOR_RED, 180, 250, 60)
+    draw_circle(COLOR_BLACK, 180, 250, 60)
+
+    fill_circle(COLOR_BLUE, 400, 250, 60)
+    draw_circle(COLOR_BLACK, 400, 250, 60)
+
+    fill_circle(COLOR_GREEN, 620, 250, 60)
+    draw_circle(COLOR_BLACK, 620, 250, 60)
+
+    draw_text("Red", COLOR_BLACK, 155, 330)
+    draw_text("Blue", COLOR_BLACK, 375, 330)
+    draw_text("Green", COLOR_BLACK, 590, 330)
+
+    refresh_screen(60)
+
+close_all_windows()
