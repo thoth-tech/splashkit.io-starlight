9395f37255557eaff699489d28221c438adcb4f9
Janki Luitel <s223165421@deakin.edu.au>
2026-04-04 12:35:17 +0000

    Add multi-language draw_line usage example

--- public/usage-examples/graphics/draw_line-1-example.cpp
+++ public/usage-examples/graphics/draw_line-1-example.cpp
@@ -2,22 +2,17 @@
 
 int main()
 {
-    open_window("Colourful Starburst", 600, 600);
-    clear_screen(COLOR_BLACK);
+    open_window("Draw Line Example", 800, 600);
 
-    // Draws starburst pattern with changing colours to window
-    draw_line(COLOR_YELLOW, 0, 0, 600, 600);
-    draw_line(COLOR_GREEN, 0, 150, 600, 450);
-    draw_line(COLOR_TEAL, 0, 300, 600, 300);
-    draw_line(COLOR_BLUE, 0, 450, 600, 150);
-    draw_line(COLOR_VIOLET, 0, 600, 600, 0);
-    draw_line(COLOR_PURPLE, 150, 0, 450, 600);
-    draw_line(COLOR_PINK, 300, 0, 300, 600);
-    draw_line(COLOR_RED, 450, 0, 150, 600);
-    draw_line(COLOR_ORANGE, 600, 0, 0, 600);
+    while (!quit_requested())
+    {
+        process_events();
+        clear_screen(COLOR_WHITE);
 
-    refresh_screen();
-    delay(5000);
-    close_all_windows();
+        draw_line(COLOR_RED, 100, 100, 700, 500);
+
+        refresh_screen(60);
+    }
+
     return 0;
 }
--- public/usage-examples/graphics/draw_line-1-example.py
+++ public/usage-examples/graphics/draw_line-1-example.py
@@ -1,19 +1,11 @@
 from splashkit import *
 
-open_window("Colourful Starburst", 600, 600)
-clear_screen(color_black())
+open_window("Draw Line Example", 800, 600)
 
-# Draws starburst pattern with changing colours to window
-draw_line(color_yellow(), 0, 0, 600, 600)
-draw_line(color_green(), 0, 150, 600, 450)
-draw_line(color_teal(), 0, 300, 600, 300)
-draw_line(color_blue(), 0, 450, 600, 150)
-draw_line(color_violet(), 0, 600, 600, 0)
-draw_line(color_purple(), 150, 0, 450, 600)
-draw_line(color_pink(), 300, 0, 300, 600)
-draw_line(color_red(), 450, 0, 150, 600)
-draw_line(color_orange(), 600, 0, 0, 600)
+while not quit_requested():
+    process_events()
+    clear_screen_to_white()
 
-refresh_screen()
-delay(5000)
-close_all_windows()
+    draw_line(color_red(), 100, 100, 700, 500)
+
+    refresh_screen_with_target_fps(60)
