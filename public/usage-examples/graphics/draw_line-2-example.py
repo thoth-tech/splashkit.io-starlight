d987167fe9a3821f3e557df076e4fc089c5d0881
Janki Luitel <s223165421@deakin.edu.au>
2026-04-12 12:56:10 +0000

    Add draw_line example as example 2

---
+++ public/usage-examples/graphics/draw_line-2-example.py
@@ -0,0 +1,13 @@
+from splashkit import *
+
+open_window("Draw Line Example", 800, 600)
+
+while not quit_requested():
+    process_events()
+    clear_screen_to_white()
+
+    draw_line(color_red(), 100, 100, 700, 500)
+
+    refresh_screen_with_target_fps(60)
+
+close_all_windows()
