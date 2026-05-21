61fac0eef3625a18850135614b848983c45d85d7
Rachel Patrao <s223955629@deakin.edu.au>
2026-05-21 23:25:06 +1000

    Add reset_layout interface usage example

---
+++ public/usage-examples/interface/reset_layout-1-example.cpp
@@ -0,0 +1,48 @@
+#include "splashkit.h"
+
+int main()
+{
+    open_window("Resetting Interface Layout", 800, 600);
+
+    set_interface_style(SHADED_LIGHT_STYLE);
+
+    while (!quit_requested())
+    {
+        process_events();
+
+        clear_screen(COLOR_WHITE);
+
+        if (start_panel("Layout Reset Demo", rectangle_from(40, 40, 500, 400)))
+        {
+            start_custom_layout();
+
+            label_element("Before reset");
+
+            split_into_columns(3);
+            set_layout_height(64);
+
+            button("One");
+            button("Two");
+            button("Three");
+
+            // Resetting returns the interface to the default single-column layout.
+            reset_layout();
+
+            label_element("Default Layout After Reset");
+
+            button("Default Layout 1");
+            button("Default Layout 2");
+            button("Default Layout 3");
+
+            end_panel("Layout Reset Demo");
+        }
+
+        draw_interface();
+
+        refresh_screen(60);
+    }
+
+    close_all_windows();
+
+    return 0;
+}
