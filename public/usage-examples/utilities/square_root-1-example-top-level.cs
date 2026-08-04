24498d6e8bbe938c0db4d1b614ef472e8ceaee91
abdullah12121212 <abdullahalanazi859@gmail.com>
2026-08-04 17:08:19 +1000

    Add square_root usage example for utilities API

---
+++ public/usage-examples/utilities/square_root-1-example-top-level.cs
@@ -0,0 +1,17 @@
+using static SplashKitSDK.SplashKit;
+
+// Find the square root of 25
+double result1 = SquareRoot(25);
+WriteLine("Square root of 25 is: " + result1);
+
+// Find the square root of 144
+double result2 = SquareRoot(144);
+WriteLine("Square root of 144 is: " + result2);
+
+// Find the square root of 2 (irrational)
+double result3 = SquareRoot(2);
+WriteLine("Square root of 2 is: " + result3);
+
+// Find the square root of 0
+double result4 = SquareRoot(0);
+WriteLine("Square root of 0 is: " + result4);
