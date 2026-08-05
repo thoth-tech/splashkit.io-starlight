f77501ad24b4f58431e1b419acaada7dd55ed90e
abdullah12121212 <abdullahalanazi859@gmail.com>
2026-08-05 13:51:28 +1000

    Fix C++ bool output and regenerate screenshot
    Changed C++ is_number example to use if/else to print "True"/"False"
    instead of to_string(bool) which prints "1"/"0". Regenerated
    screenshot from Python output to match all four language versions.

--- public/usage-examples/utilities/is_number-1-example.cpp
+++ public/usage-examples/utilities/is_number-1-example.cpp
@@ -3,20 +3,28 @@
 int main()
 {
     // Check if "42" is a number
-    bool result1 = is_number("42");
-    write_line("Is '42' a number? " + to_string(result1));
+    if (is_number("42"))
+        write_line("Is '42' a number? True");
+    else
+        write_line("Is '42' a number? False");
 
     // Check if "3.14" is a number
-    bool result2 = is_number("3.14");
-    write_line("Is '3.14' a number? " + to_string(result2));
+    if (is_number("3.14"))
+        write_line("Is '3.14' a number? True");
+    else
+        write_line("Is '3.14' a number? False");
 
     // Check if "hello" is a number
-    bool result3 = is_number("hello");
-    write_line("Is 'hello' a number? " + to_string(result3));
+    if (is_number("hello"))
+        write_line("Is 'hello' a number? True");
+    else
+        write_line("Is 'hello' a number? False");
 
     // Check if "12abc" is a number
-    bool result4 = is_number("12abc");
-    write_line("Is '12abc' a number? " + to_string(result4));
+    if (is_number("12abc"))
+        write_line("Is '12abc' a number? True");
+    else
+        write_line("Is '12abc' a number? False");
 
     return 0;
 }
