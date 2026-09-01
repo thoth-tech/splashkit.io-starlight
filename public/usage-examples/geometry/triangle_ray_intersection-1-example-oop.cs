fcb3d9f8ac2ef79265326a468916ebe521e36b5b
jasveena15 <kaurjasveena15@gmail.com>
2026-09-02 02:54:39 +1000

    Update triangle_ray_intersection-1-example-oop.cs

--- public/usage-examples/geometry/triangle_ray_intersection-1-example-oop.cs
+++ public/usage-examples/geometry/triangle_ray_intersection-1-example-oop.cs
@@ -1,95 +1,98 @@
 using SplashKitSDK;
 
-public class Program
+namespace TriangleRayIntersectionExample
 {
-    public static void Main()
+    public class Program
     {
-        SplashKit.OpenWindow("Triangle Ray Intersection", 800, 600);
-
-        Point2D rayOrigin = SplashKit.PointAt(120, 300);
-
-        Triangle tri = SplashKit.TriangleFrom(
-            SplashKit.PointAt(500, 180),
-            SplashKit.PointAt(650, 420),
-            SplashKit.PointAt(420, 420)
-        );
-
-        while (!SplashKit.QuitRequested())
+        public static void Main()
         {
-            SplashKit.ProcessEvents();
+            SplashKit.OpenWindow("Triangle Ray Intersection", 800, 600);
 
-            Point2D mouse = SplashKit.MousePosition();
+            Point2D rayOrigin = SplashKit.PointAt(120, 300);
 
-            Vector2D heading = SplashKit.VectorTo(
-                mouse.X - rayOrigin.X,
-                mouse.Y - rayOrigin.Y
+            Triangle tri = SplashKit.TriangleFrom(
+                SplashKit.PointAt(500, 180),
+                SplashKit.PointAt(650, 420),
+                SplashKit.PointAt(420, 420)
             );
 
-            bool intersects = SplashKit.TriangleRayIntersection(
-                rayOrigin,
-                heading,
-                tri
-            );
-
-            SplashKit.ClearScreen(Color.White);
-
-            if (intersects)
+            while (!SplashKit.QuitRequested())
             {
-                SplashKit.FillTriangle(Color.Green, tri);
+                SplashKit.ProcessEvents();
+
+                Point2D mouse = SplashKit.MousePosition();
+
+                Vector2D heading = SplashKit.VectorTo(
+                    mouse.X - rayOrigin.X,
+                    mouse.Y - rayOrigin.Y
+                );
+
+                bool intersects = SplashKit.TriangleRayIntersection(
+                    rayOrigin,
+                    heading,
+                    tri
+                );
+
+                SplashKit.ClearScreen(Color.White);
+
+                if (intersects)
+                {
+                    SplashKit.FillTriangle(Color.Green, tri);
+
+                    SplashKit.DrawText(
+                        "Ray intersects the triangle",
+                        Color.Green,
+                        20,
+                        20
+                    );
+                }
+                else
+                {
+                    SplashKit.FillTriangle(Color.Red, tri);
+
+                    SplashKit.DrawText(
+                        "Ray does not intersect the triangle",
+                        Color.Red,
+                        20,
+                        20
+                    );
+                }
+
+                SplashKit.DrawTriangle(Color.Black, tri);
+
+                SplashKit.DrawCircle(
+                    Color.Blue,
+                    rayOrigin.X,
+                    rayOrigin.Y,
+                    6
+                );
+
+                double rayLength = 1000;
+
+                Point2D rayEnd = SplashKit.PointAt(
+                    rayOrigin.X + heading.X * rayLength,
+                    rayOrigin.Y + heading.Y * rayLength
+                );
+
+                SplashKit.DrawLine(
+                    Color.Blue,
+                    rayOrigin.X,
+                    rayOrigin.Y,
+                    rayEnd.X,
+                    rayEnd.Y
+                );
 
                 SplashKit.DrawText(
-                    "Ray intersects the triangle",
-                    Color.Green,
+                    "Move the mouse to change the ray direction",
+                    Color.Black,
                     20,
-                    20
+                    550
                 );
-            }
-            else
-            {
-                SplashKit.FillTriangle(Color.Red, tri);
 
-                SplashKit.DrawText(
-                    "Ray does not intersect the triangle",
-                    Color.Red,
-                    20,
-                    20
-                );
+                SplashKit.RefreshScreen(60);
             }
 
-            SplashKit.DrawTriangle(Color.Black, tri);
-
-            SplashKit.DrawCircle(
-                Color.Blue,
-                rayOrigin.X,
-                rayOrigin.Y,
-                6
-            );
-
-            double rayLength = 1000;
-
-            Point2D rayEnd = SplashKit.PointAt(
-                rayOrigin.X + heading.X * rayLength,
-                rayOrigin.Y + heading.Y * rayLength
-            );
-
-            SplashKit.DrawLine(
-                Color.Blue,
-                rayOrigin.X,
-                rayOrigin.Y,
-                rayEnd.X,
-                rayEnd.Y
-            );
-
-            SplashKit.DrawText(
-                "Move the mouse to change the ray direction",
-                Color.Black,
-                20,
-                550
-            );
-
-            SplashKit.RefreshScreen(60);
+            SplashKit.CloseAllWindows();
         }
-
-        SplashKit.CloseAllWindows();
     }
 }
